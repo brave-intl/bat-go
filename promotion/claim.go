1e48da92ac1118826beb436306eeebec6d11d865
Michael McLaughlin <4896982+maikelmclauflin@users.noreply.github.com>
2019-10-22 12:20:52 -0500

    added constraint (#102)

--- promotion/claim.go
+++ promotion/claim.go
@@ -70,7 +70,7 @@ type Claim struct {
 	ApproximateValue decimal.Decimal `db:"approximate_value"`
 	Redeemed         bool            `db:"redeemed"`
 	Bonus            decimal.Decimal `db:"bonus"`
-	Platform         *string         `db:"platform"`
+	Platform         string          `db:"platform"`
 }
 
 // ClaimCreds encapsulates the credentials to be signed in response to a valid claim
