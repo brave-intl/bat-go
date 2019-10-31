42bc8d95aa60af924559bf69c1b851ffeca9c510
eV <ev@7pr.xyz>
2019-10-31 10:35:53 +0000

    allow rate limit bypass for token authorized clients

--- middleware/rate_limiter.go
+++ middleware/rate_limiter.go
@@ -10,7 +10,7 @@ import (
 
 // RateLimiter rate limits the number of requests a
 // user from a single IP address can make
-func RateLimiter() func(http.Handler) http.Handler {
+func RateLimiter(next http.Handler) http.Handler {
 	store, err := memstore.New(65536)
 	if err != nil {
 		log.Fatal(err)
@@ -31,6 +31,12 @@ func RateLimiter() func(http.Handler) http.Handler {
 			Path:       true,
 		},
 	}
-
-	return httpRateLimiter.RateLimit
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !isSimpleTokenInContext(r.Context()) {
+			httpRateLimiter.RateLimit(next).ServeHTTP(w, r)
+		} else {
+			// override rate limiting for authorized endpoints
+			next.ServeHTTP(w, r)
+		}
+	})
 }
