fe941b7304ae5cc7386b161b2615decac4a94b96
Michael McLaughlin <mmclaughlin@brave.com>
2019-12-12 10:58:25 -0600

    added metrics

--- promotion/claim.go
+++ promotion/claim.go
@@ -10,6 +10,7 @@ import (
 	"github.com/jmoiron/sqlx/types"
 	"github.com/lib/pq"
 	"github.com/pkg/errors"
+	"github.com/prometheus/client_golang/prometheus"
 	uuid "github.com/satori/go.uuid"
 	"github.com/shopspring/decimal"
 )
@@ -179,6 +180,14 @@ func (service *Service) ClaimPromotionForWallet(
 			raven.CaptureErrorAndWait(err, nil)
 		}
 	}
+
+	value, _ := claim.ApproximateValue.Float64()
+	labels := prometheus.Labels{
+		"platform": promotion.Platform,
+		"type":     promotion.Type,
+	}
+	countGrantsClaimedTotal.With(labels).Inc()
+	countGrantsClaimedBatTotal.With(labels).Add(value)
 
 	go func() {
 		_, err := service.RunNextClaimJob(ctx)
