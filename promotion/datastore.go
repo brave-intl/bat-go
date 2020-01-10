6f9661323535bf6dbb30508c797d2ffca5f639b9
Cory McDonald <cmcdonald@brave.com>
2020-01-10 12:36:44 -0600

    Add rollbacks

--- promotion/datastore.go
+++ promotion/datastore.go
@@ -170,6 +170,7 @@ func (pg *Postgres) CreateOrder(totalPrice decimal.Decimal, merchantID string, s
 		totalPrice, merchantID, status)
 
 	if err != nil {
+		tx.Rollback()
 		return nil, err
 	}
 
@@ -184,11 +185,13 @@ func (pg *Postgres) CreateOrder(totalPrice decimal.Decimal, merchantID string, s
 		err = nstmt.Get(&orderItems[i], orderItems[i])
 
 		if err != nil {
+			tx.Rollback()
 			return nil, err
 		}
 	}
 	err = tx.Commit()
 	if err != nil {
+		tx.Rollback()
 		return nil, err
 	}
 
