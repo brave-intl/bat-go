8225e32706119624c1d3c0aefb22d393dad6a851
Ben Huson <husobee@e-ie.io>
2020-03-30 14:38:57 -0400

    linting issues

--- utils/handlers/healthcheck.go
+++ utils/handlers/healthcheck.go
@@ -49,6 +49,7 @@ func HealthCheckHandler(version, buildTime, commit string) http.HandlerFunc {
 func PingHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain")
 	w.WriteHeader(http.StatusOK)
+
 	if _, err := w.Write([]byte(".")); err != nil {
 		log.Printf("failed to write response to writer: %s", err)
 	}
