f50f525942e074f1fa31fccf49e4e501a328080d
Ben Huson <husobee@e-ie.io>
2020-04-14 15:15:37 -0400

    adding templated email generation, particularly the month/year of the email

---
+++ cmd/settlement_paypal.go
@@ -0,0 +1,308 @@
+package cmd
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+	"text/template"
+	"time"
+
+	"github.com/brave-intl/bat-go/settlement"
+	"github.com/brave-intl/bat-go/settlement/paypal"
+	"github.com/brave-intl/bat-go/utils/closers"
+	"github.com/gocarina/gocsv"
+	"github.com/shopspring/decimal"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	input    string
+	currency string
+	txnID    string
+	auth     string
+	rate     float64
+	out      string
+)
+
+func init() {
+	// add complete and transform subcommand
+	paypalSettlementCmd.AddCommand(completePaypalSettlementCmd)
+	paypalSettlementCmd.AddCommand(transformPaypalSettlementCmd)
+	paypalSettlementCmd.AddCommand(emailPaypalSettlementCmd)
+
+	// add this command as a settlement subcommand
+	settlementCmd.AddCommand(paypalSettlementCmd)
+
+	// setup the flags
+
+	// input (required by all)
+	paypalSettlementCmd.PersistentFlags().StringVarP(&input, "input", "i", "",
+		"the file or comma delimited list of files that should be utilized")
+	viper.BindPFlag("input", paypalSettlementCmd.PersistentFlags().Lookup("input"))
+	viper.BindEnv("input", "INPUT")
+	paypalSettlementCmd.MarkPersistentFlagRequired("input")
+
+	// out (required by all with default)
+	paypalSettlementCmd.PersistentFlags().StringVarP(&out, "out", "o", "./paypal-settlement",
+		"the location of the file")
+	viper.BindPFlag("out", paypalSettlementCmd.PersistentFlags().Lookup("out"))
+	viper.BindEnv("out", "OUT")
+
+	// currency (required by transform)
+	transformPaypalSettlementCmd.PersistentFlags().StringVarP(&currency, "currency", "c", "",
+		"a currency must be set")
+	viper.BindPFlag("currency", transformPaypalSettlementCmd.PersistentFlags().Lookup("currency"))
+	viper.BindEnv("currency", "CURRENCY")
+	transformPaypalSettlementCmd.MarkPersistentFlagRequired("currency")
+
+	// txnID (required by complete)
+	completePaypalSettlementCmd.PersistentFlags().StringVarP(&txnID, "txn-id", "t", "",
+		"the completed mass pay transaction id")
+	viper.BindPFlag("txn-id", paypalSettlementCmd.PersistentFlags().Lookup("txn-id"))
+	viper.BindEnv("txn-id", "TXN_ID")
+	completePaypalSettlementCmd.MarkPersistentFlagRequired("txn-id")
+
+	// rate-auth
+	transformPaypalSettlementCmd.PersistentFlags().StringVarP(&auth, "rate-auth", "a", "",
+		"the rate auth service")
+	viper.BindPFlag("rate-auth", paypalSettlementCmd.PersistentFlags().Lookup("rate-auth"))
+	viper.BindEnv("rate-auth", "RATE_AUTH")
+	transformPaypalSettlementCmd.MarkPersistentFlagRequired("rate-auth")
+
+	// rate
+	paypalSettlementCmd.PersistentFlags().Float64VarP(&rate, "rate", "r", 0,
+		"the rate to compute the currency conversion")
+	viper.BindPFlag("rate", paypalSettlementCmd.PersistentFlags().Lookup("rate"))
+	viper.BindEnv("rate", "RATE")
+	transformPaypalSettlementCmd.MarkPersistentFlagRequired("rate")
+}
+
+// EmailTemplate performs template replacement of date fields in emails
+func EmailTemplate(inPath string, outPath string) error {
+	// read in email template
+	data, err := ioutil.ReadFile(inPath)
+	if err != nil {
+		return fmt.Errorf("failed to read template: %w", err)
+	}
+	// perform template rendering to out
+	f, err := os.Create(outPath)
+	if err != nil {
+		return fmt.Errorf("failed to create output: %w", err)
+	}
+	defer f.Close()
+
+	var (
+		today = time.Now()
+		// template will have a "year" and "month" field
+		v = struct {
+			Month int
+			Year  int
+		}{
+			Month: int(today.Month()),
+			Year:  today.Year(),
+		}
+		t = template.Must(template.New("email").Parse(string(data)))
+	)
+
+	if err := t.Execute(f, v); err != nil {
+		return fmt.Errorf("failed to execute template: %w", err)
+	}
+	return nil
+}
+
+var (
+	paypalSettlementCmd = &cobra.Command{
+		Use:   "paypal",
+		Short: "provides paypal settlement",
+	}
+
+	emailPaypalSettlementCmd = &cobra.Command{
+		Use:   "email",
+		Short: "provides population of a templated email",
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := EmailTemplate(input, out); err != nil {
+				log.Printf("failed to perform email templating: %s\n", err)
+				os.Exit(1)
+			}
+		},
+	}
+
+	completePaypalSettlementCmd = &cobra.Command{
+		Use:   "complete",
+		Short: "provides completion of paypal settlement",
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := CompleteSettlement(input, out, txnID); err != nil {
+				log.Printf("failed to perform complete: %s\n", err)
+				os.Exit(1)
+			}
+		},
+	}
+
+	transformPaypalSettlementCmd = &cobra.Command{
+		Use:   "transform",
+		Short: "provides transform of paypal settlement for mass pay",
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := TransformForMassPay(TransformArgs{
+				In:       input,
+				Currency: currency,
+				Auth:     auth,
+				Rate:     decimal.NewFromFloat(rate),
+				Out:      out,
+			}); err != nil {
+				log.Printf("failed to perform transform: %s\n", err)
+				os.Exit(1)
+			}
+		},
+	}
+)
+
+// CompleteSettlement marks the settlement file as complete
+func CompleteSettlement(inPath string, outPath string, txnID string) error {
+	fmt.Println("RUNNING: complete")
+	if inPath == "" {
+		return errors.New("the 'in' flag must be set")
+	}
+	if txnID == "" {
+		return errors.New("the 'txnID' flag must be set")
+	}
+	if outPath == "./paypal-settlement" {
+		// use a file with extension if none is passed
+		outPath = "./paypal-settlement-complete.json"
+	}
+	payouts, err := ReadFiles(inPath)
+	if err != nil {
+		return err
+	}
+	for i, payout := range *payouts {
+		if payout.WalletProvider != "paypal" {
+			return errors.New("Error, non-paypal payment included.\nThis command should be called only on the filtered paypal-settlement.json")
+		}
+		payout.Status = "complete"
+		payout.ProviderID = txnID
+		(*payouts)[i] = payout
+	}
+	err = WriteTransactions(outPath, payouts)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// TransformArgs are the args required for the transform command
+type TransformArgs struct {
+	In       string
+	Currency string
+	Auth     string
+	Rate     decimal.Decimal
+	Out      string
+}
+
+// WriteTransactions writes settlement transactions to a json file
+func WriteTransactions(outPath string, metadata *[]settlement.Transaction) error {
+	data, err := json.MarshalIndent(metadata, "", "  ")
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(outPath, data, 0600)
+}
+
+// WriteMassPayCSV writes a csv for using with Paypal web mass payments
+func WriteMassPayCSV(outPath string, metadata *[]paypal.Metadata) error {
+	rows := []*paypal.MassPayRow{}
+	total := decimal.NewFromFloat(0)
+	currency := ""
+	for _, entry := range *metadata {
+		row := entry.ToMassPayCSVRow()
+		total = total.Add(row.Amount)
+		currency = row.Currency
+		rows = append(rows, row)
+	}
+	if len(rows) > 5000 {
+		return errors.New("a payout cannot be larger than 5000 lines items long")
+	}
+	fmt.Println("payouts", len(rows))
+	fmt.Println("total", total.String(), currency)
+
+	data, err := gocsv.MarshalString(&rows)
+	if err != nil {
+		return err
+	}
+
+	f, err := os.Create(outPath)
+	if err != nil {
+		return err
+	}
+	defer closers.Panic(f)
+	_, err = f.WriteString(data)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// TransformForMassPay starts the process to transform a settlement into a mass pay csv
+func TransformForMassPay(args TransformArgs) (err error) {
+	fmt.Println("RUNNING: transform")
+	if args.In == "" {
+		return errors.New("the 'input' flag must be set")
+	}
+	if args.Currency == "" {
+		return errors.New("the 'currency' flag must be set")
+	}
+
+	payouts, err := ReadFiles(args.In)
+	if err != nil {
+		return err
+	}
+
+	rate, err := paypal.GetRate(args.Currency, args.Rate, args.Auth)
+	if err != nil {
+		return err
+	}
+	args.Rate = rate
+
+	txs, err := paypal.CalculateTransactionAmounts(args.Currency, args.Rate, payouts)
+	if err != nil {
+		return err
+	}
+
+	err = WriteTransactions(args.Out+".json", txs)
+	if err != nil {
+		return err
+	}
+
+	metadata, err := paypal.MergeAndTransformPayouts(txs)
+	if err != nil {
+		return err
+	}
+
+	err = WriteMassPayCSV(args.Out+".csv", metadata)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ReadFiles reads a series of files
+func ReadFiles(input string) (*[]settlement.Transaction, error) {
+	var allPayouts []settlement.Transaction
+	files := strings.Split(input, ",")
+	for _, file := range files {
+		var batPayouts []settlement.Transaction
+		bytes, err := ioutil.ReadFile(file)
+		if err != nil {
+			return nil, err
+		}
+		err = json.Unmarshal(bytes, &batPayouts)
+		if err != nil {
+			return nil, err
+		}
+		allPayouts = append(allPayouts, batPayouts...)
+	}
+	return &allPayouts, nil
+}
