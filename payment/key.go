b0cf76ffe8fee66f859bdc5ed3f971a77e0ba8bc
Cory McDonald <cmcdonald@brave.com>
2020-04-27 13:15:56 -0500

    Use secretbox for encrypting

--- payment/key.go
+++ payment/key.go
@@ -1,18 +1,20 @@
 package payment
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
 	"crypto/rand"
 	"encoding/base64"
 	"encoding/hex"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 	"time"
+
+	"golang.org/x/crypto/nacl/secretbox"
 )
 
-// AESKey for encrypting secrets
-var AESKey = os.Getenv("ENCRYPTION_KEY")
+// EncryptionKey for encrypting secrets
+var EncryptionKey = os.Getenv("ENCRYPTION_KEY")
 var keyLength = 24
 
 // Key represents a merchant's keys to validate skus
@@ -42,34 +44,26 @@ func (key *Key) SetSecretKey() error {
 func encryptSecretKey(secretKey string) (secretText string, nonceString string, err error) {
 	// The key argument should be the AES key, either 16 or 32 bytes
 	// to select AES-128 or AES-256.
-	key := []byte(AESKey)
+	var encryptionKey [32]byte
+	copy(encryptionKey[:], []byte(EncryptionKey))
+
 	plaintext := []byte(secretKey)
 
-	block, err := aes.NewCipher(key)
-	if err != nil {
+	var nonce [24]byte
+	if _, err := io.ReadFull(rand.Reader, nonce[:]); err != nil {
 		return "", "", err
 	}
 
-	// Never use more than 2^32 random nonces with a given key because of the risk of a repeat.
-	nonce := make([]byte, 12)
-	if _, err := rand.Read(nonce); err != nil {
-		return "", "", err
-	}
-
-	aesgcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return "", "", err
-	}
-
-	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
+	var out []byte
+	ciphertext := secretbox.Seal(out[:], plaintext, &nonce, &encryptionKey)
 
 	return fmt.Sprintf("%x", ciphertext), fmt.Sprintf("%x", nonce), nil
 }
 
 func decryptSecretKey(encryptedSecretKey string, nonceKey string) (string, error) {
-	// The key argument should be the AES key, either 16 or 32 bytes
-	// to select AES-128 or AES-256.
-	key := []byte(AESKey)
+	var encryptionKey [32]byte
+	copy(encryptionKey[:], []byte(EncryptionKey))
+
 	ciphertext, err := hex.DecodeString(encryptedSecretKey)
 	if err != nil {
 		return "", nil
@@ -80,22 +74,14 @@ func decryptSecretKey(encryptedSecretKey string, nonceKey string) (string, error
 		return "", nil
 	}
 
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return "", err
+	var decryptNonce [24]byte
+	copy(decryptNonce[:], nonce)
+	decrypted, ok := secretbox.Open(nil, ciphertext, &decryptNonce, &encryptionKey)
+	if !ok {
+		return "", errors.New("Could not decrypt the value of the secret")
 	}
 
-	aesgcm, err := cipher.NewGCM(block)
-	if err != nil {
-		return "", err
-	}
-
-	plaintext, err := aesgcm.Open(nil, nonce, ciphertext, nil)
-	if err != nil {
-		return "", err
-	}
-
-	return string(plaintext), nil
+	return string(decrypted), nil
 }
 
 func randomString(n int) (string, error) {
