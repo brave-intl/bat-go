1ac058f531b4aef76779a4237b09c25563f52279
Ben Huson <husobee@e-ie.io>
2020-04-28 16:01:44 -0400

    updates to add amount to claim summary structure

--- promotion/claim_summary.go
+++ promotion/claim_summary.go
@@ -8,6 +8,7 @@ import (
 
 // ClaimSummary outlines the state of a wallet's claims
 type ClaimSummary struct {
+	Amount    decimal.Decimal `json:"amount" db:"amount"`
 	Earnings  decimal.Decimal `json:"earnings" db:"earnings"`
 	LastClaim time.Time       `json:"lastClaim" db:"last_claim"`
 	Type      string          `json:"type" db:"type"`
