1cc1d2472c18883f58c676e447a64372f85a72f6
husobee <husobee@e-ie.io>
2020-06-24 10:56:55 -0400

    updated v3 schema for wallet output

--- wallet/outputs.go
+++ wallet/outputs.go
@@ -13,19 +13,26 @@ const (
 	LTCCurrency     = "LTC"
 )
 
-// ProviderDetailsV3 - details about the provider
-type ProviderDetailsV3 struct {
-	Name      string `json:"provider"`
-	ID        string `json:"providerId"`
-	LinkingID string `json:"providerLinkingId"`
+// BraveProviderDetailsV3 - details about the provider
+type BraveProviderDetailsV3 struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+// UpholdProviderDetailsV3 - details about the provider
+type UpholdProviderDetailsV3 struct {
+	Name             string `json:"name"`
+	LinkingID        string `json:"linkingId"`
+	AnonymousAddress string `json:"anonymousAddress"`
 }
 
 // WalletResponseV3 - wallet creation response
 type WalletResponseV3 struct {
-	PaymentID   string            `json:"paymentId"`
-	Provider    ProviderDetailsV3 `json:"provider"`
-	AltCurrency string            `json:"altcurrency"`
-	PublicKey   string            `json:"publicKey"`
+	PaymentID              string      `json:"paymentId"`
+	DepositAccountProvider interface{} `json:"depositAccountProvider,omitempty"`
+	WalletProvider         interface{} `json:"walletProvider,omitempty"`
+	AltCurrency            string      `json:"altcurrency"`
+	PublicKey              string      `json:"publicKey"`
 }
 
 func convertAltCurrency(a *altcurrency.AltCurrency) string {
@@ -48,24 +55,42 @@ func convertAltCurrency(a *altcurrency.AltCurrency) string {
 
 func infoToResponseV3(info *walletutils.Info) WalletResponseV3 {
 	var (
-		linkingID   string
-		altCurrency string = convertAltCurrency(info.AltCurrency)
+		linkingID        string
+		anonymousAddress string
+		altCurrency      string = convertAltCurrency(info.AltCurrency)
 	)
 	if info == nil {
 		return WalletResponseV3{}
 	}
+
 	if info.ProviderLinkingID == nil {
 		linkingID = ""
+	} else {
+		linkingID = info.ProviderLinkingID.String()
 	}
 
-	return WalletResponseV3{
+	if info.AnonymousAddress == nil {
+		anonymousAddress = ""
+	} else {
+		anonymousAddress = info.AnonymousAddress.String()
+	}
+
+	resp := WalletResponseV3{
 		PaymentID:   info.ID,
 		AltCurrency: altCurrency,
 		PublicKey:   info.PublicKey,
-		Provider: ProviderDetailsV3{
-			Name:      info.Provider,
-			ID:        info.ProviderID,
-			LinkingID: linkingID,
+		WalletProvider: BraveProviderDetailsV3{
+			Name: "brave",
+			ID:   info.ProviderID,
 		},
 	}
+	// if this is linked to uphold, add the default account provider
+	if info.Provider == "uphold" {
+		resp.DepositAccountProvider = UpholdProviderDetailsV3{
+			Name:             info.Provider,
+			LinkingID:        linkingID,
+			AnonymousAddress: anonymousAddress,
+		}
+	}
+	return resp
 }
