57093188f314543b47fee683e89ecb962b8487f8
Michael McLaughlin <4896982+maikelmclauflin@users.noreply.github.com>
2020-06-25 21:53:55 -0500

    added ua checking to populate platform column (#434)
    * added ua checking to populate platform column
    
    * added another os
    
    * fixed err wrapping
    
    * used errors.is for conflict check
    
    * moved ua tests into utils folder
    
    * used different dep and cleaned up parse platform
    
    * linted

--- promotion/instrumented_datastore.go
+++ promotion/instrumented_datastore.go
@@ -326,7 +326,7 @@ func (_d DatastoreWithPrometheus) GetWallet(id uuid.UUID) (ip1 *wallet.Info, err
 }
 
 // InsertBATLossEvent implements Datastore
-func (_d DatastoreWithPrometheus) InsertBATLossEvent(ctx context.Context, paymentID uuid.UUID, reportID int, amount decimal.Decimal) (b1 bool, err error) {
+func (_d DatastoreWithPrometheus) InsertBATLossEvent(ctx context.Context, paymentID uuid.UUID, reportID int, amount decimal.Decimal, platform string) (b1 bool, err error) {
 	_since := time.Now()
 	defer func() {
 		result := "ok"
@@ -336,7 +336,7 @@ func (_d DatastoreWithPrometheus) InsertBATLossEvent(ctx context.Context, paymen
 
 		datastoreDurationSummaryVec.WithLabelValues(_d.instanceName, "InsertBATLossEvent", result).Observe(time.Since(_since).Seconds())
 	}()
-	return _d.base.InsertBATLossEvent(ctx, paymentID, reportID, amount)
+	return _d.base.InsertBATLossEvent(ctx, paymentID, reportID, amount, platform)
 }
 
 // InsertClobberedClaims implements Datastore
