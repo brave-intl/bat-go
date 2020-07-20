0e643c04bdbfb9eb973882d11746227e4a5ab1e3
husobee <husobee@e-ie.io>
2020-07-20 16:02:48 -0400

    fixing more tests

--- wallet/datastore.go
+++ wallet/datastore.go
@@ -177,7 +177,9 @@ func (pg *Postgres) GetWalletByPublicKey(pk string) (*walletutils.Info, error) {
 	statement := `
 	select
 		id, provider, provider_id, public_key, provider_linking_id, anonymous_address,
-		user_deposit_account_provider, user_deposit_account_provider_id, user_deposit_account_anonymous_address
+		coalesce(user_deposit_account_provider, '') as user_deposit_account_provider,
+		coalesce(user_deposit_account_provider_id,'') as user_deposit_account_provider_id,
+		user_deposit_account_anonymous_address
 	from
 		wallets
 	WHERE public_key = $1
@@ -192,7 +194,9 @@ func (pg *Postgres) GetByProviderLinkingID(providerLinkingID uuid.UUID) (*[]wall
 	statement := `
 	select
 		id, provider, provider_id, public_key, provider_linking_id, anonymous_address,
-		user_deposit_account_provider, user_deposit_account_provider_id, user_deposit_account_anonymous_address
+		coalesce(user_deposit_account_provider, '') as user_deposit_account_provider,
+		coalesce(user_deposit_account_provider_id,'') as user_deposit_account_provider_id,
+		user_deposit_account_anonymous_address
 	from
 		wallets
 	WHERE provider_linking_id = $1
@@ -264,7 +268,9 @@ func txGetByProviderLinkingID(tx *sqlx.Tx, providerLinkingID uuid.UUID) (*[]wall
 	statement := `
 	select
 		id, provider, provider_id, public_key, provider_linking_id, anonymous_address,
-		user_deposit_account_provider, user_deposit_account_provider_id, user_deposit_account_anonymous_address
+		coalesce(user_deposit_account_provider, '') as user_deposit_account_provider,
+		coalesce(user_deposit_account_provider_id,'') as user_deposit_account_provider_id,
+		user_deposit_account_anonymous_address
 	from
 		wallets
 	WHERE provider_linking_id = $1
