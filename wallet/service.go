784f95d74eafd576a18f00f6014a65ccd34db2d5
Michael McLaughlin <4896982+maikelmclauflin@users.noreply.github.com>
2020-07-31 09:22:25 -0500

    fixed linking (#455)
    * fixed linking
    
    * make new instrumented datastore
    
    Co-authored-by: husobee <husobee@e-ie.io>

--- wallet/service.go
+++ wallet/service.go
@@ -142,7 +142,11 @@ func (service *Service) LinkWallet(
 			return handlers.WrapError(errors.New("wallets do not match"), "unable to match wallets", http.StatusForbidden)
 		}
 	} else {
-		err := service.Datastore.LinkWallet(info.ID, providerLinkingID, anonymousAddress, depositProvider)
+		destination := info.ProviderID
+		if info.ProviderID == "" {
+			destination = tx.Destination
+		}
+		err := service.Datastore.LinkWallet(info.ID, destination, providerLinkingID, anonymousAddress, depositProvider)
 		if err != nil {
 			status := http.StatusInternalServerError
 			if err == ErrTooManyCardsLinked {
