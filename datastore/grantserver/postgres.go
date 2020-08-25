6c5d40e5e384c79d68f180835893bb29cf083f03
Nick von Pentz <nickvonpentz@gmail.com>
2020-08-25 10:47:45 -0600

    Bump current migration version in postgres.go

--- datastore/grantserver/postgres.go
+++ datastore/grantserver/postgres.go
@@ -18,7 +18,7 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
-const currentMigrationVersion = 19
+const currentMigrationVersion = 20
 
 var (
 	// dbInstanceClassToMaxConn -  https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/AuroraPostgreSQL.Managing.html
