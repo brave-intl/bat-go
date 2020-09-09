3c52948392222129827f63f8f5f6b1b317a12a86
Michael McLaughlin <mmclaughlin@brave.com>
2020-09-09 11:04:14 -0500

    added comment to explain different test for key use

--- utils/clients/gemini/client_test.go
+++ utils/clients/gemini/client_test.go
@@ -8,8 +8,11 @@ import (
 	"encoding/json"
 	"os"
 	"testing"
+	"time"
 
+	"github.com/brave-intl/bat-go/settlement"
 	"github.com/brave-intl/bat-go/utils/cryptography"
+	uuid "github.com/satori/go.uuid"
 	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/suite"
 )
@@ -39,6 +42,7 @@ func (suite *GeminiTestSuite) TestBulkPay() {
 	suite.Require().NoError(err, "Must be able to correctly initialize the client")
 	five := decimal.NewFromFloat(5)
 
+	// // uncomment if using a master key
 	// accountListRequest := suite.preparePrivateRequest(NewAccountListPayload())
 	// accounts, err := client.FetchAccountList(ctx, suite.apikey, suite.secret, accountListRequest)
 	// suite.Require().NoError(err, "should not error during account list fetching")
@@ -55,55 +59,55 @@ func (suite *GeminiTestSuite) TestBulkPay() {
 		"must have at least 5 bat to pass the rest of the test",
 	)
 
-	// tx := settlement.Transaction{
-	// 	// use this settlement id to create an ephemeral test
-	// 	// SettlementID: uuid.NewV4().String(),
-	// 	SettlementID: uuid.Must(uuid.FromString("4077459f-7389-46d3-a0d8-b1e56b2d279b")).String(),
-	// 	Destination:  os.Getenv("GEMINI_TEST_DESTINATION_ID"),
-	// 	Channel:      "brave.com",
-	// }
-	// BAT := "BAT"
-	// payouts := []PayoutPayload{{
-	// 	TxRef:       GenerateTxRef(&tx),
-	// 	Amount:      five,
-	// 	Currency:    BAT,
-	// 	Destination: tx.Destination,
-	// }}
-	// bulkPayoutRequest := suite.preparePrivateRequest(NewBulkPayoutPayload(
-	// 	os.Getenv("GEMINI_CLIENT_ID"),
-	// 	&payouts,
-	// ))
+	tx := settlement.Transaction{
+		// use this settlement id to create an ephemeral test
+		// SettlementID: uuid.NewV4().String(),
+		SettlementID: uuid.Must(uuid.FromString("4077459f-7389-46d3-a0d8-b1e56b2d279b")).String(),
+		Destination:  os.Getenv("GEMINI_TEST_DESTINATION_ID"),
+		Channel:      "brave.com",
+	}
+	BAT := "BAT"
+	payouts := []PayoutPayload{{
+		TxRef:       GenerateTxRef(&tx),
+		Amount:      five,
+		Currency:    BAT,
+		Destination: tx.Destination,
+	}}
+	bulkPayoutRequest := suite.preparePrivateRequest(NewBulkPayoutPayload(
+		os.Getenv("GEMINI_CLIENT_ID"),
+		&payouts,
+	))
 
-	// _, err = client.UploadBulkPayout(ctx, suite.apikey, suite.secret, bulkPayoutRequest)
-	// suite.Require().NoError(err, "should not error during bulk payout uploading")
+	_, err = client.UploadBulkPayout(ctx, suite.apikey, suite.secret, bulkPayoutRequest)
+	suite.Require().NoError(err, "should not error during bulk payout uploading")
 
-	// pendingStatus := "Pending"
-	// expectedPayoutResult := []PayoutResult{{
-	// 	Result:      "OK",
-	// 	TxRef:       GenerateTxRef(&tx),
-	// 	Amount:      &five,
-	// 	Currency:    &BAT,
-	// 	Destination: &tx.Destination,
-	// 	Status:      &pendingStatus,
-	// }}
-	// // suite.Require().Equal(&expectedPayoutResult, bulkPayoutResponse, "response should be predictable")
-	// completeStatus := "Completed"
-	// for {
-	// 	<-time.After(5 * time.Second)
-	// 	bulkPayoutRequest := suite.preparePrivateRequest(NewBulkPayoutPayload(
-	// 		os.Getenv("GEMINI_CLIENT_ID"),
-	// 		&payouts,
-	// 	))
+	pendingStatus := "Pending"
+	expectedPayoutResult := []PayoutResult{{
+		Result:      "OK",
+		TxRef:       GenerateTxRef(&tx),
+		Amount:      &five,
+		Currency:    &BAT,
+		Destination: &tx.Destination,
+		Status:      &pendingStatus,
+	}}
+	// suite.Require().Equal(&expectedPayoutResult, bulkPayoutResponse, "response should be predictable")
+	completeStatus := "Completed"
+	for {
+		<-time.After(5 * time.Second)
+		bulkPayoutRequest := suite.preparePrivateRequest(NewBulkPayoutPayload(
+			os.Getenv("GEMINI_CLIENT_ID"),
+			&payouts,
+		))
 
-	// 	bulkPayoutResponse, err := client.UploadBulkPayout(ctx, suite.apikey, suite.secret, bulkPayoutRequest)
-	// 	suite.Require().NoError(err, "should not error during bulk payout uploading")
-	// 	if (*(*bulkPayoutResponse)[0].Status) == completeStatus {
-	// 		// fmt.Printf("status: %s", *expectedPayoutResult[0].Status)
-	// 		expectedPayoutResult[0].Status = &completeStatus
-	// 		suite.Require().Equal(&expectedPayoutResult, bulkPayoutResponse, "success response should be predictable")
-	// 		return
-	// 	}
-	// }
+		bulkPayoutResponse, err := client.UploadBulkPayout(ctx, suite.apikey, suite.secret, bulkPayoutRequest)
+		suite.Require().NoError(err, "should not error during bulk payout uploading")
+		if (*(*bulkPayoutResponse)[0].Status) == completeStatus {
+			// fmt.Printf("status: %s", *expectedPayoutResult[0].Status)
+			expectedPayoutResult[0].Status = &completeStatus
+			suite.Require().Equal(&expectedPayoutResult, bulkPayoutResponse, "success response should be predictable")
+			return
+		}
+	}
 }
 
 func findBalanceByCurrency(balances *[]Balance, currency string) Balance {
