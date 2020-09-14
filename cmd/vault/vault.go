7e79958b8f8d71f4f74a2cb0da26174b5ee13731
Michael McLaughlin <mmclaughlin@brave.com>
2020-09-14 15:33:21 -0500

    moved key import into cmd

---
+++ cmd/vault/vault.go
@@ -0,0 +1,40 @@
+package vault
+
+import (
+	"github.com/brave-intl/bat-go/cmd"
+	"github.com/brave-intl/bat-go/settlement"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	// ConfigPath provides a path to read configuration out of
+	ConfigPath string
+
+	// Config is a configuration file to map known wallet keys to unknown wallet keys
+	Config *settlement.Config
+
+	// VaultCmd adds a command to cobra for vault interfacing
+	VaultCmd = &cobra.Command{
+		Use:   "vault",
+		Short: "provides a succinct interface with vault",
+	}
+)
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	cmd.RootCmd.AddCommand(VaultCmd)
+
+	// config - defaults to config.yaml
+	VaultCmd.PersistentFlags().StringVarP(&ConfigPath, "config", "c", "config.yaml",
+		"the default path to a configuration file")
+	cmd.Must(viper.BindPFlag("config", VaultCmd.PersistentFlags().Lookup("config")))
+	cmd.Must(viper.BindEnv("config", "CONFIG"))
+}
+
+func initConfig() {
+	var err error
+	Config, err = settlement.ReadYamlConfig(ConfigPath)
+	cmd.Must(err)
+}
