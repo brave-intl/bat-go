da28932d6acae6c3bf51eac9809650e8e6de5830
Dan A. Lipeles <danlipeles@icloud.com>
2020-11-03 22:26:17 -0500

    Alphabetize

--- cmd/serve/grant.go
+++ cmd/serve/grant.go
@@ -166,8 +166,8 @@ func setupRouter(ctx context.Context, logger *zerolog.Logger) (context.Context, 
 	// add runnable jobs:
 	jobs = append(jobs, paymentService.Jobs()...)
 
-	r.Mount("/v1/orders", payment.Router(paymentService))
 	r.Mount("/v1/credentials", payment.CredentialRouter(paymentService))
+	r.Mount("/v1/orders", payment.Router(paymentService))
 	r.Mount("/v1/votes", payment.VoteRouter(paymentService))
 
 	if os.Getenv("FEATURE_MERCHANT") != "" {
