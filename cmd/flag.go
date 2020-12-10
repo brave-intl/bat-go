f2dc10444b6ae43ba7460dba45eddc24a15dad09
Michael McLaughlin <mmclaughlin@brave.com>
2020-12-09 23:07:03 -0600

    added flag file

---
+++ cmd/flag.go
@@ -0,0 +1,97 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// FlagBuilder creates a flag builder
+type FlagBuilder struct {
+	commands []*cobra.Command
+	key      string
+}
+
+// String attaches a string flag to the command
+func (fb *FlagBuilder) String(key string, defaultValue string, description string) *FlagBuilder {
+	fb.key = key
+	fb.loopCommands(func(command *cobra.Command) {
+		command.Flags().String(key, defaultValue, description)
+		Must(viper.BindPFlag(key, command.Flags().Lookup(key)))
+	})
+	return fb
+}
+
+// Int attaches an int flag to the command
+func (fb *FlagBuilder) Int(key string, defaultValue int, description string) *FlagBuilder {
+	fb.key = key
+	fb.loopCommands(func(command *cobra.Command) {
+		command.Flags().Int(key, defaultValue, description)
+		Must(viper.BindPFlag(key, command.Flags().Lookup(key)))
+	})
+	return fb
+}
+
+// Float64 attaches a float64 type flag to the command
+func (fb *FlagBuilder) Float64(key string, defaultValue float64, description string) *FlagBuilder {
+	fb.key = key
+	fb.loopCommands(func(command *cobra.Command) {
+		command.Flags().Float64(key, defaultValue, description)
+		Must(viper.BindPFlag(key, command.Flags().Lookup(key)))
+	})
+	return fb
+}
+
+// Bool attaches a bool flag to the command
+func (fb *FlagBuilder) Bool(key string, defaultValue bool, description string) *FlagBuilder {
+	fb.key = key
+	fb.loopCommands(func(command *cobra.Command) {
+		command.Flags().Bool(key, defaultValue, description)
+		Must(viper.BindPFlag(key, command.Flags().Lookup(key)))
+	})
+	return fb
+}
+
+// Require requires the flag
+func (fb *FlagBuilder) Require() *FlagBuilder {
+	fb.loopCommands(func(command *cobra.Command) {
+		Must(command.MarkFlagRequired(fb.key))
+	})
+	return fb
+}
+
+// Env attaches an env
+func (fb *FlagBuilder) Env(env string) *FlagBuilder {
+	Must(viper.BindEnv(fb.key, env))
+	return fb
+}
+
+// NewFlagBuilder creates a new FlagBuilder from one command
+func NewFlagBuilder(command *cobra.Command) *FlagBuilder {
+	fb := FlagBuilder{}
+	if command != nil {
+		fb.AddCommand(command)
+	}
+	return &fb
+}
+
+// AddCommand adds a command
+func (fb *FlagBuilder) AddCommand(command *cobra.Command) *FlagBuilder {
+	fb.commands = append(fb.commands, command)
+	return fb
+}
+
+// Concat combine flag builders
+func (fb *FlagBuilder) Concat(builders ...*FlagBuilder) *FlagBuilder {
+	newBuilder := NewFlagBuilder(nil)
+	allBuilders := append([]*FlagBuilder{fb}, builders...)
+	for _, builder := range allBuilders {
+		fb.commands = append(fb.commands, builder.commands...)
+	}
+	return newBuilder
+}
+
+func (fb *FlagBuilder) loopCommands(iterator func(*cobra.Command)) {
+	for _, command := range fb.commands {
+		iterator(command)
+	}
+}
