2889eaffb22e432e350f1a055e11755f8e0d081c
Dan A. Lipeles <danlipeles@icloud.com>
2020-12-16 11:06:07 -0500

    Add payment microservice files

---
+++ cmd/payments/rest_run.go
@@ -0,0 +1,45 @@
+package payments
+
+import (
+
+	// pprof imports
+	_ "net/http/pprof"
+
+	"github.com/spf13/cobra"
+)
+
+// PaymentRestRun - Main entrypoint of the REST subcommand
+// This function takes a cobra command and starts up the
+// payments rest microservice.
+func PaymentRestRun(command *cobra.Command, args []string) {
+	// setup generic middlewares and routes for health-check and metrics
+	// r := cmd.SetupRouter(command.Context())
+	// r, ctx, _ := payment.SetupService(command.Context(), r)
+	// logger, err := appctx.GetLogger(ctx)
+	// cmd.Must(err)
+
+	// // add profiling flag to enable profiling routes
+	// if viper.GetString("pprof-enabled") != "" {
+	// 	// pprof attaches routes to default serve mux
+	// 	// host:6061/debug/pprof/
+	// 	go func() {
+	// 		logger.Error().Err(http.ListenAndServe(":6061", http.DefaultServeMux))
+	// 	}()
+	// }
+
+	// // setup server, and run
+	// srv := http.Server{
+	// 	Addr:         viper.GetString("address"),
+	// 	Handler:      chi.ServerBaseContext(ctx, r),
+	// 	ReadTimeout:  5 * time.Second,
+	// 	WriteTimeout: 20 * time.Second,
+	// }
+
+	// // make sure exceptions go to sentry
+	// defer sentry.Flush(time.Second * 2)
+
+	// if err = srv.ListenAndServe(); err != nil {
+	// 	sentry.CaptureException(err)
+	// 	logger.Fatal().Err(err).Msg("HTTP server start failed!")
+	// }
+}
