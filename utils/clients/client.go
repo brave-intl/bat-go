e62b9d40602241ef90afc66ca695b07c4456c8d9
Michael McLaughlin <mmclaughlin@brave.com>
2021-01-26 14:48:29 -0600

    added checks for idempotency

--- utils/clients/client.go
+++ utils/clients/client.go
@@ -215,6 +215,11 @@ func (c *SimpleHTTPClient) do(
 
 	if status >= 200 && status <= 299 {
 		if v != nil {
+			// // helpful if you want to read the body as it is
+			// bodyBytes, _ := requestutils.Read(resp.Body)
+			// resp.Body.Close() //  must close
+			// fmt.Println(string(bodyBytes))
+			// resp.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))
 			err = json.NewDecoder(resp.Body).Decode(v)
 			if err != nil {
 				return resp, errors.Wrap(err, ErrUnableToDecode)
