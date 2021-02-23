74cbacfe59b7e253c751793c78dc287a3fe2557f
Michael McLaughlin <4896982+maikelmclauflin@users.noreply.github.com>
2021-02-23 14:55:26 -0600

    Added bf flag (#693)
    * added flag
    
    * moved token fetch within flag

--- promotion/service.go
+++ promotion/service.go
@@ -199,19 +199,20 @@ func InitService(
 		return nil, err
 	}
 
+	var bfClient bitflyer.Client
 	// setup bfClient
-	bfClient, err := bitflyer.New()
-	if err != nil {
-		return nil, fmt.Errorf("failed to create bitflyer client: %w", err)
-	}
-
-	// get a fresh bf token
-	// this will set the AuthToken on the client for us
-	_, err = bfClient.RefreshToken(ctx, bitflyer.TokenPayloadFromCtx(ctx))
-
-	if err != nil {
-		// we don't want to stop the world if we can't connect to bf
-		logger.Error().Err(err).Msg("failed to get bf access token!")
+	if os.Getenv("BITFLYER_ENABLED") == "true" {
+		bfClient, err = bitflyer.New()
+		if err != nil {
+			return nil, fmt.Errorf("failed to create bitflyer client: %w", err)
+		}
+		// get a fresh bf token
+		// this will set the AuthToken on the client for us
+		_, err = bfClient.RefreshToken(ctx, bitflyer.TokenPayloadFromCtx(ctx))
+		if err != nil {
+			// we don't want to stop the world if we can't connect to bf
+			logger.Error().Err(err).Msg("failed to get bf access token!")
+		}
 	}
 
 	reputationClient, err := reputation.New()
