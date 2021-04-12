62e53390e5c04baea474cb44890c0029d0f3480c
husobee <husobee@e-ie.io>
2021-04-12 12:45:01 -0400

    Remove wallets feature flag (#763)
    * fixes: #579 - remove wallets-feature-flag
    
    * remove env variables not required rom docker-compose

--- wallet/service.go
+++ wallet/service.go
@@ -270,50 +270,47 @@ func SetupService(ctx context.Context, r *chi.Mux) (*chi.Mux, context.Context, *
 
 	ctx = context.WithValue(ctx, appctx.ReputationClientCTXKey, s.repClient)
 
-	// if feature is enabled, setup the routes
-	if viper.GetBool("wallets-feature-flag") {
-		// setup our wallet routes
-		r.Route("/v3/wallet", func(r chi.Router) {
-			// rate limited to 2 per minute...
-			// create wallet routes for our wallet providers
-			r.Post("/uphold", middleware.RateLimiter(ctx, 2)(middleware.InstrumentHandlerFunc(
-				"CreateUpholdWallet", CreateUpholdWalletV3)).ServeHTTP)
-			r.Post("/brave", middleware.RateLimiter(ctx, 2)(middleware.InstrumentHandlerFunc(
-				"CreateBraveWallet", CreateBraveWalletV3)).ServeHTTP)
+	// setup our wallet routes
+	r.Route("/v3/wallet", func(r chi.Router) {
+		// rate limited to 2 per minute...
+		// create wallet routes for our wallet providers
+		r.Post("/uphold", middleware.RateLimiter(ctx, 2)(middleware.InstrumentHandlerFunc(
+			"CreateUpholdWallet", CreateUpholdWalletV3)).ServeHTTP)
+		r.Post("/brave", middleware.RateLimiter(ctx, 2)(middleware.InstrumentHandlerFunc(
+			"CreateBraveWallet", CreateBraveWalletV3)).ServeHTTP)
 
-			// if wallets are being migrated we do not want to over claim, we might go over the limit
-			if viper.GetBool("enable-link-drain-flag") {
-				// create wallet claim routes for our wallet providers
-				r.Post("/uphold/{paymentID}/claim", middleware.InstrumentHandlerFunc(
-					"LinkUpholdDepositAccount", LinkUpholdDepositAccountV3(s)))
-				r.Post("/bitflyer/{paymentID}/claim", middleware.HTTPSignedOnly(s)(middleware.InstrumentHandlerFunc(
-					"LinkBitFlyerDepositAccount", LinkBitFlyerDepositAccountV3(s))).ServeHTTP)
-				r.Post("/brave/{paymentID}/claim", middleware.HTTPSignedOnly(s)(middleware.InstrumentHandlerFunc(
-					"LinkBraveDepositAccount", LinkBraveDepositAccountV3(s))).ServeHTTP)
-			}
-			// support only APIs to assist in linking limit issues
-			/*
-				TODO: currently commented out due to concerns about how to enable/disable particular
-				people from accessing this endpoint.
-				r.Post("/{custodian}/increase-limit/{custodian_id}", middleware.SimpleTokenAuthorizedOnly(
-					middleware.InstrumentHandlerFunc("IncreaseLinkingLimit", IncreaseLinkingLimitV3(s))).ServeHTTP)
-			*/
+		// if wallets are being migrated we do not want to over claim, we might go over the limit
+		if viper.GetBool("enable-link-drain-flag") {
+			// create wallet claim routes for our wallet providers
+			r.Post("/uphold/{paymentID}/claim", middleware.InstrumentHandlerFunc(
+				"LinkUpholdDepositAccount", LinkUpholdDepositAccountV3(s)))
+			r.Post("/bitflyer/{paymentID}/claim", middleware.HTTPSignedOnly(s)(middleware.InstrumentHandlerFunc(
+				"LinkBitFlyerDepositAccount", LinkBitFlyerDepositAccountV3(s))).ServeHTTP)
+			r.Post("/brave/{paymentID}/claim", middleware.HTTPSignedOnly(s)(middleware.InstrumentHandlerFunc(
+				"LinkBraveDepositAccount", LinkBraveDepositAccountV3(s))).ServeHTTP)
+		}
+		// support only APIs to assist in linking limit issues
+		/*
+			TODO: currently commented out due to concerns about how to enable/disable particular
+			people from accessing this endpoint.
+			r.Post("/{custodian}/increase-limit/{custodian_id}", middleware.SimpleTokenAuthorizedOnly(
+				middleware.InstrumentHandlerFunc("IncreaseLinkingLimit", IncreaseLinkingLimitV3(s))).ServeHTTP)
+		*/
 
-			// linking info api is okay to expose publically
-			r.Get("/linking-info",
-				middleware.InstrumentHandlerFunc("GetLinkingInfo", GetLinkingInfoV3(s)).ServeHTTP)
+		// linking info api is okay to expose publically
+		r.Get("/linking-info",
+			middleware.InstrumentHandlerFunc("GetLinkingInfo", GetLinkingInfoV3(s)).ServeHTTP)
 
-			// get wallet routes
-			r.Get("/{paymentID}", middleware.InstrumentHandlerFunc(
-				"GetWallet", GetWalletV3))
-			r.Get("/recover/{publicKey}", middleware.InstrumentHandlerFunc(
-				"RecoverWallet", RecoverWalletV3))
+		// get wallet routes
+		r.Get("/{paymentID}", middleware.InstrumentHandlerFunc(
+			"GetWallet", GetWalletV3))
+		r.Get("/recover/{publicKey}", middleware.InstrumentHandlerFunc(
+			"RecoverWallet", RecoverWalletV3))
 
-			// get wallet balance routes
-			r.Get("/uphold/{paymentID}", middleware.InstrumentHandlerFunc(
-				"GetUpholdWalletBalance", GetUpholdWalletBalanceV3))
-		})
-	}
+		// get wallet balance routes
+		r.Get("/uphold/{paymentID}", middleware.InstrumentHandlerFunc(
+			"GetUpholdWalletBalance", GetUpholdWalletBalanceV3))
+	})
 	return r, ctx, s
 }
 
