ca855bdf2c326f70e0448cfd824f303eb5bff0ab
Dan A. Lipeles <danlipeles@icloud.com>
2021-04-19 13:08:56 -0400

    Update migration version too

--- datastore/grantserver/postgres.go
+++ datastore/grantserver/postgres.go
@@ -23,7 +23,7 @@ import (
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
-const currentMigrationVersion = 33
+const currentMigrationVersion = 34
 
 var (
 	// dbInstanceClassToMaxConn -  https://docs.aws.amazon.com/AmazonRDS/latest/AuroraUserGuide/AuroraPostgreSQL.Managing.html
