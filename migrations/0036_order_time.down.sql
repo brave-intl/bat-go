012d4f0859d205d3c0abc772f8aa0421163b4cc0
husobee <husobee@e-ie.io>
2021-07-30 11:48:16 -0400

    rename table to make more sense

--- migrations/0036_order_time.down.sql
+++ migrations/0036_order_time.down.sql
@@ -2,4 +2,4 @@ alter table orders drop column valid_for;
 alter table orders drop column expires_at;
 alter table order_items drop column valid_for;
 
-drop table order_renewal;
+drop table order_payments;
