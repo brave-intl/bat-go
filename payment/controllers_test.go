e3bb287171347de6d082a3cc116b6768dd5fab6e
husobee <husobee@e-ie.io>
2021-09-20 12:29:12 -0400

    missing one call to depr method

--- payment/controllers_test.go
+++ payment/controllers_test.go
@@ -636,7 +636,12 @@ func (suite *ControllersTestSuite) fetchTimeLimitedCredentials(ctx context.Conte
 
 	isoD, err := timeutils.ParseDuration("P1M") // 1 month from the sku
 	suite.Require().NoError(err)
-	validFor, err := isoD.Base(time.Now())
+
+	ft, err := isoD.FromNow()
+	suite.Require().NoError(err)
+
+	validFor := time.Until(*ft)
+
 	suite.Require().NoError(err)
 
 	// validate we get the right number of creds back, 1 per day
