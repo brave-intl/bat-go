7d7d4b0702947e9e8a6dfcbe8d699cb0a338a6f6
eV <ev@7pr.xyz>
2021-09-27 20:04:56 +0000

    err.Error()...

--- middleware/http_signed.go
+++ middleware/http_signed.go
@@ -54,7 +54,7 @@ func VerifyHTTPSignedOnly(verifier httpsignature.ParameterizedKeystoreVerifier) 
 			ctx, keyID, err := verifier.VerifyRequest(r)
 
 			if err != nil {
-				http.Error(w, err, http.StatusForbidden)
+				http.Error(w, err.Error(), http.StatusForbidden)
 				return
 			}
 
