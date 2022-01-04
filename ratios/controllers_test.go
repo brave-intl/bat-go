69872b9df9469a5582b71131f6a5ca1bb399ca4e
husobee <husobee@e-ie.io>
2022-01-04 14:16:33 -0500

    tests for ratios endpoints

--- ratios/controllers_test.go
+++ ratios/controllers_test.go
@@ -3,18 +3,28 @@
 package ratios_test
 
 import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 
 	"github.com/asaskevich/govalidator"
 	"github.com/brave-intl/bat-go/ratios"
+	appctx "github.com/brave-intl/bat-go/utils/context"
+	logutils "github.com/brave-intl/bat-go/utils/logging"
+	"github.com/go-chi/chi"
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/suite"
 )
 
 type ControllersTestSuite struct {
+	suite.Suite
+
+	ctx      context.Context
 	service  *ratios.Service
 	mockCtrl *gomock.Controller
-	suite.Suite
 }
 
 func TestControllersTestSuite(t *testing.T) {
@@ -22,6 +32,38 @@ func TestControllersTestSuite(t *testing.T) {
 }
 
 func (suite *ControllersTestSuite) SetupSuite() {
+	// setup the context
+	suite.ctx = context.Background()
+
+	// setup debug for client
+	suite.ctx = context.WithValue(suite.ctx, appctx.DebugLoggingCTXKey, true)
+	// setup debug log level
+	suite.ctx = context.WithValue(suite.ctx, appctx.LogLevelCTXKey, "debug")
+
+	// setup a logger and put on context
+	suite.ctx, _ = logutils.SetupLogger(suite.ctx)
+
+	// setup server location
+	suite.ctx = context.WithValue(suite.ctx, appctx.CoingeckoServerCTXKey, "https://api.coingecko.com")
+	// setup token (using public api for tests)
+	suite.ctx = context.WithValue(suite.ctx, appctx.CoingeckoAccessTokenCTXKey, "")
+	// coin limit
+	suite.ctx = context.WithValue(suite.ctx, appctx.CoingeckoCoinLimitCTXKey, 2)
+	// vs-currency limit
+	suite.ctx = context.WithValue(suite.ctx, appctx.CoingeckoVsCurrencyLimitCTXKey, 2)
+
+	var redisAddr string = "redis://grant-redis:6379"
+	if len(os.Getenv("REDIS_ADDR")) > 0 {
+		redisAddr = os.Getenv("REDIS_ADDR")
+	}
+
+	suite.ctx = context.WithValue(suite.ctx, appctx.RatiosRedisAddrCTXKey, redisAddr)
+
+	// setup service now
+	var err error
+	suite.ctx, suite.service, err = ratios.InitService(suite.ctx)
+	suite.Require().NoError(err, "failed to setup ratios service")
+
 	govalidator.SetFieldsRequiredByDefault(true)
 }
 
@@ -33,5 +75,69 @@ func (suite *ControllersTestSuite) AfterTest(sn, tn string) {
 	suite.mockCtrl.Finish()
 }
 
-func (suite *ControllersTestSuite) TestCreateOrder() {
+func (suite *ControllersTestSuite) TestGetHistoryHandler() {
+
+	handler := ratios.GetHistoryHandler(suite.service)
+
+	// new request for relative handler
+	req, err := http.NewRequest("GET", "/v2/history/coingecko/{coinID}/{vsCurrency}/{duration}", nil)
+	suite.Require().NoError(err)
+
+	// add in our suite ctx
+	req = req.WithContext(suite.ctx)
+
+	rctx := chi.NewRouteContext()
+	rctx.URLParams.Add("coinID", "bat")
+	rctx.URLParams.Add("vsCurrency", "usd")
+	rctx.URLParams.Add("duration", "1d")
+
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+	// validate response code matches
+	suite.Require().Equal(http.StatusOK, rr.Code)
+
+	// example
+	// {"payload":{"prices":[[...
+	var resp = new(ratios.HistoryResponse)
+	err = json.Unmarshal(rr.Body.Bytes(), resp)
+	suite.Require().NoError(err)
+
+	suite.Require().True(len(resp.Payload.Prices) > 0)
+}
+
+func (suite *ControllersTestSuite) TestGetRelativeHandler() {
+
+	handler := ratios.GetRelativeHandler(suite.service)
+
+	// new request for relative handler
+	req, err := http.NewRequest("GET", "/v2/relative/provider/coingecko/{coinIDs}/{vsCurrencies}/{duration}", nil)
+	suite.Require().NoError(err)
+
+	rctx := chi.NewRouteContext()
+	rctx.URLParams.Add("coinIDs", "bat")
+	rctx.URLParams.Add("vsCurrencies", "usd")
+	rctx.URLParams.Add("duration", "1d")
+
+	// add in our suite ctx
+	req = req.WithContext(suite.ctx)
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+	// validate response code matches
+	suite.Require().Equal(http.StatusOK, rr.Code)
+
+	// example
+	// {"payload":{"bat":{"usd":1.3,"usd_timeframe_change":0.8356218194962891}}
+	var resp = new(ratios.RelativeResponse)
+	err = json.Unmarshal(rr.Body.Bytes(), resp)
+	suite.Require().NoError(err)
+
+	v, okBat := resp.Payload["bat"]
+	suite.Require().True(okBat)
+
+	_, okUsd := v["usd"]
+	suite.Require().True(okUsd)
 }
