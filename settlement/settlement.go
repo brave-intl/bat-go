b4294bfb38ae85c7e63492b53a0d82402fd2ef9c
eV <ev@7pr.xyz>
2022-01-06 01:18:15 +0000

    add purpose and beneficiary fields for Uphold prepared transfers

--- settlement/settlement.go
+++ settlement/settlement.go
@@ -164,7 +164,7 @@ func PrepareTransactions(wallet *uphold.Wallet, settlements []Transaction) error
 		if len(settlement.Note) > 0 {
 			message = settlement.Note
 		}
-		tx, err := wallet.PrepareTransaction(*settlement.AltCurrency, settlement.Probi, settlement.Destination, message)
+		tx, err := wallet.PrepareTransaction(*settlement.AltCurrency, settlement.Probi, settlement.Destination, message, "payout", &uphold.Beneficiary{Relationship: "business"})
 		if err != nil {
 			return err
 		}
