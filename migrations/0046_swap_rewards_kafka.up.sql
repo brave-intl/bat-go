ddc8a4ead51dc81c5ae57b93f3aaa11b28abbe63
irogatko <irogatko@brave.com>
2022-03-01 15:36:00 -0800

    sentinel to upload due to migration constraints

--- migrations/0046_swap_rewards_kafka.up.sql
+++ migrations/0046_swap_rewards_kafka.up.sql
@@ -1,4 +1,4 @@
 --- swap promotions grant reward fields for kafka worker
 alter table claims add column transaction_key uuid;
 create unique index unique_claim_transaction_key on claims(transaction_key) where transaction_key is not NULL;
-insert into wallets (id, provider, provider_id, public_key) values ('00000000-0000-0000-0000-000000000002', 'brave', '-', '-');
+insert into wallets (id, provider, provider_id, public_key) values ('00000000-0000-0000-0000-000000000002', 'uphold', '-', '-');
