8ea6862d152e3f216d8331d10e59511e15448d27
husobee <husobee@e-ie.io>
2022-03-31 08:51:55 -0400

    go1.18 and sans for test certs (#1339)

--- test/secrets/generate-client.sh
+++ test/secrets/generate-client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 openssl genrsa -out consumer.client.key 1024
-openssl req -key consumer.client.key -new -out consumer.client.req -subj '/CN=consumer.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US'
-openssl x509 -req -CA snakeoil-ca-1.crt -CAkey snakeoil-ca-1.key -in consumer.client.req -out consumer-ca1-signed.pem -days 9999 -CAcreateserial
+openssl req -key consumer.client.key -new -out consumer.client.req -subj '/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US' -addext "subjectAltName = DNS:consumer.test.confluent.io"
+openssl x509 -req -CA snakeoil-ca-1.crt -CAkey snakeoil-ca-1.key -in consumer.client.req -out consumer-ca1-signed.pem -days 9999 -CAcreateserial -extfile v3.ext
