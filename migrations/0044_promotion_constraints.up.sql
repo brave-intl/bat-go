deab40507bc1fddd4f4994f8e59bff32dec8b9ad
husobee <husobee@e-ie.io>
2022-04-12 16:49:49 -0400

    allow vBAT values up to 1 BAT

--- migrations/0044_promotion_constraints.up.sql
+++ migrations/0044_promotion_constraints.up.sql
@@ -1,3 +1,4 @@
-ALTER TABLE promotions ADD CONSTRAINT check_value_suggest_per_grant CHECK (approximate_value = 20.0 and suggestions_per_grant = 80.0);
+-- allow vBAT values up to 1 BAT
+ALTER TABLE promotions ADD CONSTRAINT check_value_suggest_per_grant CHECK ((approximate_value / suggestions_per_grant) < 1.0);
 ALTER TABLE promotions ALTER COLUMN approximate_value SET DEFAULT 20.0;
 ALTER TABLE promotions ALTER COLUMN suggestions_per_grant SET DEFAULT 80.0;
