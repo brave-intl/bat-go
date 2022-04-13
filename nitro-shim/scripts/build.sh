af923c22f5dafe78d8eb437c9a3eb6989924f628
husobee <husobee@e-ie.io>
2022-04-13 13:46:40 -0400

    add subservice support for images in building enclave

--- nitro-shim/scripts/build.sh
+++ nitro-shim/scripts/build.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 docker_image_base="${1}"
-and_run="${2}"
+
+# service var is the service we wish to run in the enclave
+service=""
+if [ "${2}" != "" ]; then 
+    service = "/${2}"
+fi
+
+and_run="${3}"
 
 set -euxo pipefail
 
-docker_image=$(docker images | grep "${docker_image_base}" | awk '{printf "%s:%s", $1, $2}')
+docker_image=$(docker images | grep "${docker_image_base}" | awk -v s="${service}" '{printf "%s%s:%s", $1, s, $2}')
 
 nitro-cli build-enclave --docker-uri ${docker_image} --output-file nitro-image.eif
 
