26f275d473e7a5fa6f8b549bcf8db60bbcb98f4f
husobee <husobee@e-ie.io>
2022-04-13 14:47:57 -0400

    fixing build script to just get one lastest image

--- nitro-shim/scripts/build.sh
+++ nitro-shim/scripts/build.sh
@@ -13,7 +13,7 @@ and_run="${3}"
 set -euxo pipefail
 
 # get the latest docker image of the base image we are looking for
-docker_image=$(docker images --format "{{.Repository}} {{.Tag}} {{.CreatedAt}}" | sort -rk 3 | awk -v s="${service}" '{printf "%s%s:%s", $1, s, $2}' | grep "${docker_image_base}")
+docker_image=$(docker images --format "{{.Repository}} {{.Tag}} {{.CreatedAt}}" | grep "${docker_image_base}" | sort -rk 3 | awk -v s="${service}" 'NR==1{printf "%s%s:%s", $1, s, $2}')
 
 nitro-cli build-enclave --docker-uri ${docker_image} --output-file nitro-image.eif
 
