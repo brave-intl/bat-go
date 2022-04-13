21310f82b0e6a62f9becac11f251058338d7bb35
husobee <husobee@e-ie.io>
2022-04-13 16:04:28 -0400

    shim needs to login to repository

--- nitro-shim/scripts/build.sh
+++ nitro-shim/scripts/build.sh
@@ -13,9 +13,14 @@ and_run="${3}"
 set -euxo pipefail
 
 # get the latest docker image of the base image we are looking for
-docker_image=$(docker images --format "{{.Repository}} {{.Tag}} {{.CreatedAt}}" | grep "${docker_image_base}" | sort -rk 3 | awk -v s="${service}" 'NR==1{printf "%s%s:%s", $1, s, $2}')
+docker_image=$(docker images --format "{{.Repository}} {{.CreatedAt}}" | grep "${docker_image_base}" | sort -rk 2 | awk -v s="${service}" 'NR==1{printf "%s%s", $1, s}')
 
-nitro-cli build-enclave --docker-uri ${docker_image} --output-file nitro-image.eif
+# get the latest docker image of the base image we are looking for with tag
+docker_image_tag=$(docker images --format "{{.Repository}} {{.Tag}} {{.CreatedAt}}" | grep "${docker_image_base}" | sort -rk 3 | awk -v s="${service}" 'NR==1{printf "%s%s:%s", $1, s, $2}')
+
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin ${docker_image}
+
+nitro-cli build-enclave --docker-uri ${docker_image_tag} --output-file nitro-image.eif
 
 if [ "${and_run}" == "run" ]; then 
   /enclave/run.sh
