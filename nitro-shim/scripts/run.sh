d3198696864cd6b6d9373aa09fdbc02a249979d1
husobee <husobee@e-ie.io>
2022-04-14 08:59:06 -0400

    script perms, comments

--- nitro-shim/scripts/run.sh
+++ nitro-shim/scripts/run.sh
@@ -5,8 +5,10 @@ set -euxo pipefail
 nitro-cli run-enclave --cpu-count 2 --memory 512 ---eif-path nitro-image.eif > /tmp/output.json
 cat /tmp/output.json
 
+# background the proxy startup
 /enclave/start-proxies.sh &
 
 # sleep forever while enclave runs
+# will cause the container to die if enclave dies
 /enclave/sleep.sh
 
