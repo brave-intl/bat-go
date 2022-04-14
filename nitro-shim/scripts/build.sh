f1b2724fb64ff653aa86aa3813cf1734ec2cced1
husobee <husobee@e-ie.io>
2022-04-14 08:50:01 -0400

    reorganizing build script

--- nitro-shim/scripts/build.sh
+++ nitro-shim/scripts/build.sh
@@ -12,16 +12,17 @@ and_run="${3}"
 
 set -euxo pipefail
 
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin ${docker_image}
+
+# wait for a few seconds for eks to pull down the right version
+sleep 20
+
+
 # get the latest docker image of the base image we are looking for
 docker_image=$(docker images --format "{{.Repository}} {{.CreatedAt}}" | grep "${docker_image_base}" | sort -rk 2 | awk -v s="${service}" 'NR==1{printf "%s%s", $1, s}')
 
 # get the latest docker image of the base image we are looking for with tag
 docker_image_tag=$(docker images --format "{{.Repository}} {{.Tag}} {{.CreatedAt}}" | grep "${docker_image_base}" | sort -rk 3 | awk -v s="${service}" 'NR==1{printf "%s%s:%s", $1, s, $2}')
-
-aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin ${docker_image}
-
-# wait for a few minutes
-sleep 120
 
 nitro-cli build-enclave --docker-uri ${docker_image_tag} --output-file nitro-image.eif
 
