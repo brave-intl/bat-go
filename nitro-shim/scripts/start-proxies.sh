2d1a1c36b3ecca28305e41c98f883604c65c793d
Philipp Winter <phw@brave.com>
2022-04-14 17:23:52 -0700

    Set up proxy tools for ia2.
    The service ia2 needs a viproxy configuration that differs from our
    payment service.  It also needs socksproxy and kafkaproxy.

--- nitro-shim/scripts/start-proxies.sh
+++ nitro-shim/scripts/start-proxies.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+service="${1}"
 CID=""
+PARENT_CID="3" # the CID of the EC2 instance
 
 # first get the cid of the enclave
 # wait for enclave to startup
@@ -14,10 +16,30 @@ do
         break
 done
 
-# at this point the enclave is up
-# setup inbound traffic proxy
-export IN_ADDRS=":8080"
-export OUT_ADDRS="${CID}:8080"
+# at this point the enclave is up.  depending on what service we're running,
+# it's now time to set up proxy tools
+if [ "${service}" = "payments" ]; then
+    # setup inbound traffic proxy
+    export IN_ADDRS=":8080"
+    export OUT_ADDRS="${CID}:8080"
+elif [ "${service}" = "ia2" ]; then
+    # setup proxy that allows the enclave to talk to Let's Encrypt and our Kafka
+    # cluster
+    export SOCKS_PROXY_ALLOWED_FQDNS="acme-v02.api.letsencrypt.org,${KAFKA_BROKERS}"
+    export SOCKS_PROXY_LISTEN_ADDR="127.0.0.1:1080"
+    /enclave/socksproxy > /tmp/socksproxy.log &
+
+    # setup proxy that serves as an HTTP-to-Kafka bridge
+    export KAFKA_KEY_PATH="/etc/kafka/secrets/key"
+    export KAFKA_CERT_PATH="/etc/kafka/secrets/certificate"
+    export KAFKA_PROXY_LISTEN_ADDR="127.0.0.1:8081"
+    export KAFKA_TOPIC="ip_addr_anon.dev.repsys.upstream"
+    /enclave/kafkaproxy > /tmp/kafkaproxy.log &
+
+    # setup proxy for inbound traffic, ACME, and the enclave's SOCKS proxy
+    export IN_ADDRS=":8080,:80,${PARENT_CID}:80"
+    export OUT_ADDRS="${CID}:8080,${CID}:80,127.0.0.1:1080"
+fi
 
 # next startup the proxy
 /enclave/viproxy > /tmp/viproxy.log &
