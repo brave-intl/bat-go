bf562e351ab8abd6e63ff95121a22bca8abffae8
clD11 <23483715+clD11@users.noreply.github.com>
2022-07-31 11:08:23 +0100

    feat: time limited v2 skus api
    updated order creds to many

--- migrations/0046_sub_issuer_creds.up.sql
+++ migrations/0046_sub_issuer_creds.up.sql
@@ -1,3 +1,7 @@
+alter table order_creds add column id uuid not null default uuid_generate_v4();
+alter table order_creds drop constraint order_creds_pkey;
+alter table order_creds add primary key (id);
+
 alter table order_creds add column valid_from timestamp with time zone default null;
 alter table order_creds add column valid_to timestamp with time zone default null;
 alter table order_creds add column credential_type text default null;
