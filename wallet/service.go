5447be318f5c0eb4db30546aaaa53f08af8456fc
husobee <husobee@e-ie.io>
2022-08-02 08:36:13 -0400

    correcting the error for mismatched regions

--- wallet/service.go
+++ wallet/service.go
@@ -162,9 +162,7 @@ func (service *Service) LinkBitFlyerWallet(ctx context.Context, walletID uuid.UU
 			return handlers.WrapError(err, "unable to link - unusual activity", http.StatusBadRequest)
 		}
 		if errors.Is(err, ErrGeoResetDifferent) {
-			return handlers.WrapError(errors.New("wallets do not match"), "mismatched provider accounts", http.StatusForbidden)
-			// TODO: eventual correct this behavior when client can support
-			// return handlers.WrapError(err, "unable to link - geo-reset different", http.StatusBadRequest)
+			return handlers.WrapError(err, "mismatched provider account regions", http.StatusBadRequest)
 		}
 		return handlers.WrapError(err, "unable to link bitflyer wallets", status)
 	}
@@ -200,9 +198,7 @@ func (service *Service) LinkGeminiWallet(ctx context.Context, walletID uuid.UUID
 			return handlers.WrapError(err, "unable to link - unusual activity", http.StatusBadRequest)
 		}
 		if errors.Is(err, ErrGeoResetDifferent) {
-			return handlers.WrapError(errors.New("wallets do not match"), "mismatched provider accounts", http.StatusForbidden)
-			// TODO: eventual correct this behavior when client can support
-			// return handlers.WrapError(err, "unable to link - geo-reset different", http.StatusBadRequest)
+			return handlers.WrapError(err, "mismatched provider account regions", http.StatusBadRequest)
 		}
 		return handlers.WrapError(err, "unable to link gemini wallets", status)
 	}
@@ -274,9 +270,7 @@ func (service *Service) LinkWallet(
 			return handlers.WrapError(err, "unable to link - unusual activity", http.StatusBadRequest)
 		}
 		if errors.Is(err, ErrGeoResetDifferent) {
-			return handlers.WrapError(errors.New("wallets do not match"), "mismatched provider accounts", http.StatusForbidden)
-			// TODO: eventual correct this behavior when client can support
-			// return handlers.WrapError(err, "unable to link - geo-reset different", http.StatusBadRequest)
+			return handlers.WrapError(err, "mismatched provider account regions", http.StatusBadRequest)
 		}
 		return handlers.WrapError(err, "unable to link uphold wallets", status)
 	}
@@ -427,9 +421,7 @@ func (service *Service) LinkBraveWallet(ctx context.Context, from, to uuid.UUID)
 			return handlers.WrapError(err, "unable to link - unusual activity", http.StatusBadRequest)
 		}
 		if errors.Is(err, ErrGeoResetDifferent) {
-			return handlers.WrapError(errors.New("wallets do not match"), "mismatched provider accounts", http.StatusForbidden)
-			// TODO: eventual correct this behavior when client can support
-			// return handlers.WrapError(err, "unable to link - geo-reset different", http.StatusBadRequest)
+			return handlers.WrapError(err, "mismatched provider account regions", http.StatusBadRequest)
 		}
 		return handlers.WrapError(err, "unable to link brave wallets", status)
 	}
