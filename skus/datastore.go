e471db3ba544932aabfe52556422bb186e0379de
husobee <husobee@e-ie.io>
2022-08-10 10:16:03 -0400

    explicit metadata key/value type

--- skus/datastore.go
+++ skus/datastore.go
@@ -1426,7 +1426,7 @@ func (pg *Postgres) AppendOrderMetadata(ctx context.Context, orderID *uuid.UUID,
 	if err != nil {
 		return err
 	}
-	stmt := `update orders set metadata = metadata || jsonb_build_object($1, $2), updated_at = current_timestamp where id = $3`
+	stmt := `update orders set metadata = metadata || jsonb_build_object($1::text, $2::text), updated_at = current_timestamp where id = $3`
 
 	result, err := tx.Exec(stmt, key, value, orderID.String())
 	if err != nil {
