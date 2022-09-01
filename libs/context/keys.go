1baa4e3d87ac6a7998b9ef8982f9f90a0a9e805b
clD11 <23483715+clD11@users.noreply.github.com>
2022-09-01 15:23:07 +0100

    feat: wallet creation for rewards 2.5
    removed duplicate

--- libs/context/keys.go
+++ libs/context/keys.go
@@ -166,15 +166,11 @@ const (
 	// RequireUpholdCountryCTXKey - the context key for getting the rate limit
 	RequireUpholdCountryCTXKey CTXKey = "require_uphold_country"
 
-	// WalletGeolocationDisabledBucketCTXKey the context key for getting the S3 bucket for wallet creation disabled geolocations.
-	WalletGeolocationDisabledBucketCTXKey CTXKey = "disabled-wallet-geolocations-bucket"
-	// WalletGeolocationDisabledCTXKey the context key for getting the s3 object for wallet creation disabled geolocations.
-	WalletGeolocationDisabledCTXKey CTXKey = "disabled-wallet-geolocations.json"
+	// DisabledWalletGeolocationsCTXKey context key used to retrieve the S3 object name for disabled wallet geolocations.
+	DisabledWalletGeolocationsCTXKey CTXKey = "disabled_wallet_geolocations"
 
 	// PlaystoreJSONKeyCTXKey - the context key for playstore json key
 	PlaystoreJSONKeyCTXKey CTXKey = "playstore_json_key"
-	// DisabledWalletGeolocationsCTXKey context key used to retrieve the S3 object name for disabled wallet geolocations.
-	DisabledWalletGeolocationsCTXKey CTXKey = "disabled_wallet_geolocations"
 )
 
 var (
