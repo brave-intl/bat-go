ff5a36badd3512a0d6d60ad4bb381211bd0d48a8
Nick von Pentz <nickvonpentz@protonmail.com>
2022-09-19 15:47:19 -0400

    Add logic that writes email status notifications into Dynamo DB

--- serverless/email/notification/main.go
+++ serverless/email/notification/main.go
@@ -6,23 +6,46 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/aws/aws-sdk-go/aws"
+	appaws "github.com/brave-intl/bat-go/libs/aws"
 	"github.com/brave-intl/bat-go/libs/logging"
+	"github.com/google/uuid"
 	"github.com/rs/zerolog"
 )
 
 var (
+	// setup context/logger
 	ctx, logger = logging.SetupLoggerWithLevel(context.Background(), zerolog.InfoLevel)
+
+	// tables
+	statusTable = aws.String("status")
+
+	// clients
+	dynamoClient *dynamodb.Client
 )
+
+func init() {
+	// setup base aws config
+	config, err := appaws.BaseAWSConfig(ctx, logger)
+	if err != nil {
+		panic("failed to create aws config")
+	}
+	// setup dynamodb client
+	dynamoClient = dynamodb.NewFromConfig(config)
+}
 
 // We use event publishing to receive delivery status notifications via SNS
 // https://docs.aws.amazon.com/ses/latest/dg/event-publishing-retrieving-sns-contents.html
 type sesNotification struct {
 	EventType string `json:"eventType"`
-	Mail      mail   `json:"mail"`
+	Mail      *mail  `json:"mail"`
 }
 
 type mail struct {
-	MessageID string `json:"messageId"`
+	MessageID string              `json:"messageId"`
+	Tags      []map[string]string `json:"tags"`
 }
 
 func handler(ctx context.Context, snsEvent events.SNSEvent) {
@@ -38,8 +61,32 @@ func handler(ctx context.Context, snsEvent events.SNSEvent) {
 			continue
 		}
 
-		// TODO Write notificationType, timestamp to database, using messageID as key
-		// TODO Should these be batch written outside of the loop?
+		// Get Idempotency ID from tags to use as partition key, skip if it is not present
+		var idempotencyID string
+		for _, tag := range notification.Mail.Tags {
+			idempotencyID, _ = tag["idempotencyID"]
+		}
+
+		if idempotencyID == "" {
+			logger.Warn().Msg("missing idempotency ID from email " + notification.Mail.MessageID)
+			continue
+		}
+
+		_, err = dynamoClient.PutItem(ctx, &dynamodb.PutItemInput{
+			TableName: statusTable,
+			Item: map[string]types.AttributeValue{
+				"idempotency_id": &types.AttributeValueMemberS{Value: idempotencyID},
+				"status_id":      &types.AttributeValueMemberS{Value: uuid.NewString()},
+				"message_id":     &types.AttributeValueMemberS{Value: notification.Mail.MessageID},
+				"type":           &types.AttributeValueMemberS{Value: notification.EventType},
+			},
+		})
+
+		if err != nil {
+			logger.Error().Err(err).Msg(
+				"failed to write status to dynamodb for messageID " + notification.Mail.MessageID,
+			)
+		}
 	}
 }
 
