731be540d7d45cb98740cf071cf9b38f80382dc8
husobee <husobee@e-ie.io>
2022-09-20 17:27:32 -0400

    fixing up error responses, and logging for email webhook lambda

--- serverless/email/webhook/input.go
+++ serverless/email/webhook/input.go
@@ -1,9 +1,5 @@
 package main
 
-import (
-	"github.com/google/uuid"
-)
-
 /*
 	Supported resource types:
 		DEPOSIT_CRYPTO
@@ -18,7 +14,7 @@ type emailPayload struct {
 	ClientUserID string                 `json:"clientUserId" valid:"-"`
 	AccountID    int64                  `json:"accountId" valid:"-"`
 	Timestamp    int64                  `json:"timestamp" valid:"-"`
-	UUID         uuid.UUID              `json:"uuid" valid:"uuid"`
+	UUID         string                 `json:"uuid" valid:"uuid"`
 	ResourceType string                 `json:"resourceType" valid:"in(DEPOSIT_CRYPTO|SUPPORT_TICKET|KYC_LEVEL|DEPOSIT_FIAT|ACCOUNT_FROZEN)"`
 	Data         map[string]interface{} `json:"data" valid:"-"`
 }
--- serverless/email/webhook/main.go
+++ serverless/email/webhook/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"errors"
-	"fmt"
 	"net/http"
 	"os"
 	"strings"
@@ -13,11 +11,13 @@ import (
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
 	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
 	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
 	"github.com/aws/aws-sdk-go-v2/service/ses"
 	sestypes "github.com/aws/aws-sdk-go-v2/service/ses/types"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
 	appaws "github.com/brave-intl/bat-go/libs/aws"
 	"github.com/brave-intl/bat-go/libs/logging"
 	"github.com/google/uuid"
@@ -30,6 +30,7 @@ var (
 	namespaceArn  = os.Getenv("EMAIL_UUID_NAMESPACE")
 	authTokensArn = os.Getenv("AUTH_TOKENS")
 	configSet     = os.Getenv("SES_CONFIG_SET")
+	dynamoRoleArn = os.Getenv("DYNAMODB_ROLE_ARN")
 
 	// setup context/logger
 	ctx, logger = logging.SetupLoggerWithLevel(context.Background(), zerolog.InfoLevel)
@@ -55,23 +56,39 @@ func init() {
 	if err != nil {
 		panic("failed to create aws config")
 	}
-	// setup dynamodb client
-	dynamoClient = dynamodb.NewFromConfig(config)
 	// setup ses client
 	sesClient = ses.NewFromConfig(config)
 	// setup secrets manager client
 	secretsManagerClient = secretsmanager.NewFromConfig(config)
 
-	logger.Info().Msg("aws clients are setup")
+	// setup base aws dynamo config
+	dynConfig, err := appaws.BaseAWSConfig(ctx, logger)
+	if err != nil {
+		panic("failed to create aws dynamo config")
+	}
+
+	// sts assume creds
+	stsClient := sts.NewFromConfig(config)
+	creds := stscreds.NewAssumeRoleProvider(stsClient, dynamoRoleArn)
+	dynConfig.Credentials = creds
+
+	// setup dynamodb client
+	dynamoClient = dynamodb.NewFromConfig(dynConfig)
 
 	namespaceSecretOutput, err = secretsManagerClient.GetSecretValue(ctx, &secretsmanager.GetSecretValueInput{
 		SecretId: aws.String(namespaceArn),
 	})
-	logger.Info().Err(err).Msg("got namespace secret output")
+	if err != nil {
+		logger.Error().Err(err).Msg("failed to get namespace from secrets manager")
+		panic("failed to get namespace cannot start service")
+	}
 	authTokenSecretOutput, err = secretsManagerClient.GetSecretValue(ctx, &secretsmanager.GetSecretValueInput{
 		SecretId: aws.String(authTokensArn),
 	})
-	logger.Info().Err(err).Msg("got auth tokens secret output")
+	if err != nil {
+		logger.Error().Err(err).Msg("failed to get auth token from secrets manager")
+		panic("failed to get auth tokens cannot start service")
+	}
 }
 
 // handler takes the api gateway request and sends a templated email
@@ -83,28 +100,25 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 
 	// no api key in request
 	if !authOK {
-		logger.Info().Msg("there is no api key in request, returning unauthorized")
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusUnauthorized,
 			Body:       http.StatusText(http.StatusUnauthorized),
-		}, errors.New("authentication key missing in request")
+		}, nil
 	}
 
 	// check auth token against our comma seperated list of valid auth tokens
 	for _, token := range strings.Split(*authTokenSecretOutput.SecretString, ",") {
 		if apiKey == token {
-			logger.Info().Msg("successfully matched the token")
 			authenticated = true
 		}
 	}
 
 	// api key in request does not match any configured
 	if !authenticated {
-		logger.Info().Msg("request is not authenticated")
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusUnauthorized,
 			Body:       http.StatusText(http.StatusUnauthorized),
-		}, errors.New("failed to match authentication token")
+		}, nil
 	}
 
 	// handler accepts from the request event the payload
@@ -112,40 +126,39 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	payload := new(emailPayload)
 	err := json.Unmarshal([]byte(request.Body), payload)
 	if err != nil {
-		logger.Info().Err(err).Msg("failed to unmarshal request body")
+		logger.Error().Err(err).Msg("failed to unmarshal request body")
 		// failed to unmarshal request appropriately
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusBadRequest,
 			Body:       http.StatusText(http.StatusBadRequest),
-		}, fmt.Errorf("failed to unmarshal request body: %w", err)
+		}, nil
 	}
 
 	// perform input payload validation
 	valid, err := govalidator.ValidateStruct(payload)
 	if err != nil {
-		logger.Info().Err(err).Msg("failed to validate the body structure")
+		logger.Error().Err(err).Msg("failed to validate the body structure")
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
 			Body:       http.StatusText(http.StatusInternalServerError),
-		}, fmt.Errorf("failed to validate input payload: %w", err)
+		}, nil
 	}
 
 	if !valid {
-		logger.Info().Msg("request payload is invalid")
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusBadRequest,
 			Body:       http.StatusText(http.StatusBadRequest),
-		}, errors.New("invalid input payload")
+		}, nil
 	}
 
 	// parse the namespace
 	namespace, err := uuid.Parse(*namespaceSecretOutput.SecretString)
 	if err != nil {
-		logger.Info().Err(err).Msg("failed to parse the namespace into a uuid")
+		logger.Error().Err(err).Msg("failed to parse the namespace into a uuid")
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
 			Body:       http.StatusText(http.StatusInternalServerError),
-		}, errors.New("misconfiguration of namespace")
+		}, nil
 	}
 
 	unsubscribeRef := uuid.NewSHA1(namespace, []byte(payload.Email)).String()
@@ -160,17 +173,17 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		ConsistentRead: aws.Bool(true), // consistent read
 	})
 	if err != nil {
-		logger.Info().Err(err).Msg("failed to get item from dynamodb")
+		logger.Error().Err(err).Msg("failed to get item from dynamodb")
 		// failed to get the base aws config
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
 			Body:       http.StatusText(http.StatusInternalServerError),
-		}, fmt.Errorf("failed to get from dynamodb: %w", err)
+		}, nil
 	}
 
 	// check if it exists, if we should not send email, they unsubscribed
 	if len(dynGetOut.Item) > 0 {
-		logger.Info().Msg("found an unsubscriber")
+		logger.Error().Msg("duplicate request")
 		// return ok
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusOK,
@@ -182,17 +195,17 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	dynGetOut, err = dynamoClient.GetItem(ctx, &dynamodb.GetItemInput{
 		TableName: idempotencyTable,
 		Key: map[string]types.AttributeValue{
-			"id": &types.AttributeValueMemberS{Value: payload.UUID.String()},
+			"id": &types.AttributeValueMemberS{Value: payload.UUID},
 		},
 		ConsistentRead: aws.Bool(true), // consistent read
 	})
 	if err != nil {
-		logger.Info().Err(err).Msg("failed to get idempotency key from dynamodb")
+		logger.Error().Err(err).Msg("failed to get idempotency key from dynamodb")
 		// failed to get the base aws config
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
 			Body:       http.StatusText(http.StatusInternalServerError),
-		}, fmt.Errorf("failed to get from dynamodb: %w", err)
+		}, nil
 	}
 
 	// check if it exists, if so we already processed
@@ -210,15 +223,14 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	// marshal template data into json
 	data, err := json.Marshal(payload.Data)
 	if err != nil {
-		logger.Info().Err(err).Msg("failed to marshal data from payload")
+		logger.Error().Err(err).Msg("failed to marshal data from payload")
 		// failed to unmarshal request appropriately
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusBadRequest,
 			Body:       http.StatusText(http.StatusBadRequest),
-		}, fmt.Errorf("failed to marshal ses template: %w", err)
+		}, nil
 	}
 
-	logger.Info().Msg("attempting to send email")
 	// send email get ses message id
 	sesOut, err := sesClient.SendTemplatedEmail(ctx, &ses.SendTemplatedEmailInput{
 		Destination: &sestypes.Destination{
@@ -232,16 +244,16 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 		Tags: []sestypes.MessageTag{
 			{
 				Name:  aws.String("idempotencyKey"),
-				Value: aws.String(payload.UUID.String()),
+				Value: aws.String(payload.UUID),
 			},
 		},
 	})
 	if err != nil {
-		logger.Info().Err(err).Msg("failed to send templated email")
+		logger.Error().Err(err).Msg("failed to send templated email")
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
 			Body:       http.StatusText(http.StatusInternalServerError),
-		}, fmt.Errorf("failed to send templated email: %w", err)
+		}, nil
 	}
 
 	// set the message id
@@ -251,19 +263,18 @@ func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyRespo
 	_, err = dynamoClient.PutItem(ctx, &dynamodb.PutItemInput{
 		TableName: idempotencyTable,
 		Item: map[string]types.AttributeValue{
-			"id":         &types.AttributeValueMemberS{Value: payload.UUID.String()},
+			"id":         &types.AttributeValueMemberS{Value: payload.UUID},
 			"message_id": &types.AttributeValueMemberS{Value: messageID},
 		},
 	})
 	if err != nil {
-		logger.Info().Err(err).Msg("failed to put idempotency key in dynamo")
+		logger.Error().Err(err).Msg("failed to put idempotency key in dynamo")
 		return events.APIGatewayProxyResponse{
 			StatusCode: http.StatusInternalServerError,
 			Body:       http.StatusText(http.StatusInternalServerError),
-		}, fmt.Errorf("failed to write to dynamodb: %w", err)
+		}, nil
 	}
 
-	logger.Info().Msg("successful response")
 	return events.APIGatewayProxyResponse{
 		StatusCode: http.StatusOK,
 		Body:       http.StatusText(http.StatusOK),
