766b3de98607e92f91aa19c6cefc1ce7bcae7d89
husobee <husobee@e-ie.io>
2023-02-16 10:24:04 -0500

    adding attestation in libs for nitro

---
+++ libs/nitro/attestation.go
@@ -0,0 +1,38 @@
+package nitro
+
+import (
+	"errors"
+
+	"github.com/hf/nsm"
+	"github.com/hf/nsm/request"
+)
+
+// Attest takes as input a nonce, user-provided data and a public key, and then
+// asks the Nitro hypervisor to return a signed attestation document that
+// contains all three values.
+func Attest(nonce, userData, publicKey []byte) ([]byte, error) {
+	s, err := nsm.OpenDefaultSession()
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err = s.Close(); err != nil {
+			elog.Printf("Attestation: Failed to close default NSM session: %s", err)
+		}
+	}()
+
+	res, err := s.Send(&request.Attestation{
+		Nonce:     nonce,
+		UserData:  userData,
+		PublicKey: publicKey,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	if res.Attestation == nil || res.Attestation.Document == nil {
+		return nil, errors.New("NSM device did not return an attestation")
+	}
+
+	return res.Attestation.Document, nil
+}
