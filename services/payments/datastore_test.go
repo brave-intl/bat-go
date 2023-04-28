1ace4ea69fb8cf71b055027a6fc9bd7082e0fda9
Jackson <jegan@brave.com>
2023-04-27 23:40:58 -0400

    Remove generated tests and fix idempotency test

--- services/payments/datastore_test.go
+++ services/payments/datastore_test.go
@@ -5,13 +5,12 @@ import (
 
 	"github.com/amazon-ion/ion-go/ion"
 	"github.com/google/uuid"
-	"github.com/shopspring/decimal"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestIdempotencyKeyGeneration(t *testing.T) {
-	to, err := uuid.Parse("")
-	from, err := uuid.Parse("")
+	to, err := uuid.Parse("683bc9ba-497a-47a5-9587-3bd03fd722bd")
+	from, err := uuid.Parse("af68d02a-907f-4e9a-8f74-b54c7629412b")
 	if err != nil {
 		panic("failed to parse test UUIDs")
 	}
@@ -28,54 +27,5 @@ func TestIdempotencyKeyGeneration(t *testing.T) {
 		Signature:           "",
 		PublicKey:           "",
 	}
-	assert.Equal(t, transaction.deriveIdempotencyKey(), "")
-}
-
-func Test_fromIonDecimal(t *testing.T) {
-	type args struct {
-		v *ion.Decimal
-	}
-	var tests []struct {
-		name string
-		args args
-		want *decimal.Decimal
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			assert.Equalf(t, tt.want, fromIonDecimal(tt.args.v), "fromIonDecimal(%v)", tt.args.v)
-		})
-	}
-}
-
-func TestIdempotencyKeyIsValid(t *testing.T) {
-	type args struct {
-		txn   *Transaction
-		entry *QLDBPaymentTransitionHistoryEntry
-	}
-	var tests []struct {
-		name string
-		args args
-		want bool
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			assert.Equalf(t, tt.want, idempotencyKeyIsValid(tt.args.txn, tt.args.entry), "idempotencyKeyIsValid(%v, %v)", tt.args.txn, tt.args.entry)
-		})
-	}
-}
-
-func TestToIonDecimal(t *testing.T) {
-	type args struct {
-		v *decimal.Decimal
-	}
-	var tests []struct {
-		name string
-		args args
-		want *ion.Decimal
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			assert.Equalf(t, tt.want, toIonDecimal(tt.args.v), "toIonDecimal(%v)", tt.args.v)
-		})
-	}
+	assert.Equal(t, transaction.deriveIdempotencyKey(), "inWrgd0F_tFjI_thCMPDmgxGeUM=")
 }
