ae4e90f30b66732c09b28a507a46c997e9035ae9
PavelBrm <pburmistrov@brave.com>
2023-05-05 17:24:44 +1200

    Remove empty repository for now

--- services/skus/db/repository/repository.go
+++ services/skus/db/repository/repository.go
@@ -1,8 +1,2 @@
 // Package repository provides access to data available in SQL-based data store.
 package repository
-
-import "context"
-
-type Orders struct{}
-
-func (r *Orders) Get(ctx context.Context)
