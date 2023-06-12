e29e124060cd78f29f8e20e3bd9eb29b463b3d39
Jackson <jegan@brave.com>
2023-06-12 15:03:22 -0400

    Remove Sprintf where not needed in payment errors

--- services/payments/errors.go
+++ services/payments/errors.go
@@ -1,24 +1,20 @@
 package payments
 
-import (
-	"fmt"
-)
-
 type QLDBReocrdNotFoundError struct{}
 
 func (e *QLDBReocrdNotFoundError) Error() string {
-	return fmt.Sprintf("QLDB record not found")
+	return "QLDB record not found"
 }
 
 // ErrInvalidVerifier is an error stating the keyID is not a valid verifier
 type ErrInvalidVerifier struct{}
 
 func (e *ErrInvalidVerifier) Error() string {
-	return fmt.Sprint("not a valid verifier")
+	return "not a valid verifier"
 }
 
 type InsufficientAuthorizationsError struct{}
 
 func (e *InsufficientAuthorizationsError) Error() string {
-	return fmt.Sprintf("insufficient authorizations")
+	return "insufficient authorizations"
 }
