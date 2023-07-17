fa234f5fe96ce5f0e062bd3421736d0baefb5577
husobee <husobee@e-ie.io>
2023-07-17 09:42:58 -0400

    add timestamps to the nitro-shim log output (#1894)

--- nitro-shim/scripts/sleep.sh
+++ nitro-shim/scripts/sleep.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+echo --- Monitoring enclave $(date) ---
 set -eux
 
 while true
 do
         # check every so often that the enclave is running
         sleep 480
+        date
 
         EID=$(nitro-cli describe-enclaves | jq -r .[].EnclaveID)
         if [ "${EID}" == "" ]; then
