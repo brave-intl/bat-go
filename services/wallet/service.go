57bdefd77245674501e574a8d91be7c68b3f4b6f
husobee <husobee@e-ie.io>
2023-08-03 07:49:27 -0400

    fixing how to grab iat/exp from claims of token

--- services/wallet/service.go
+++ services/wallet/service.go
@@ -7,6 +7,7 @@ import (
 	"encoding/hex"
 	"errors"
 	"fmt"
+	"math"
 	"net/http"
 	"os"
 	"strconv"
@@ -466,25 +467,26 @@ func (service *Service) LinkZebPayWallet(ctx context.Context, walletID uuid.UUID
 		return handlers.WrapError(errors.New(msg), msg, http.StatusBadRequest)
 	}
 
+	fmt.Println(claims)
 	// validate token (checks not before, expires with no leeway)
-	iat, ok := claims["iat"].(int64)
+	iat, ok := claims["iat"].(float64)
 	if !ok {
 		const msg = "zebpay linking info validation failed no iat"
 		return handlers.WrapError(errors.New(msg), msg, http.StatusBadRequest)
 	}
 
-	exp, ok := claims["exp"].(int64)
+	exp, ok := claims["exp"].(float64)
 	if !ok {
 		const msg = "zebpay linking info validation failed no exp"
 		return handlers.WrapError(errors.New(msg), msg, http.StatusBadRequest)
 	}
 
-	if time.Now().Before(time.Unix(iat, 0)) {
+	if time.Now().Before(time.Unix(int64(math.Round(iat)), 0)) {
 		const msg = "zebpay linking info validation failed issued at is after now"
 		return handlers.WrapError(errors.New(msg), msg, http.StatusBadRequest)
 	}
 
-	if time.Now().After(time.Unix(exp, 0)) {
+	if time.Now().After(time.Unix(int64(math.Round(exp)), 0)) {
 		const msg = "zebpay linking info validation failed expired is before now"
 		return handlers.WrapError(errors.New(msg), msg, http.StatusBadRequest)
 	}
