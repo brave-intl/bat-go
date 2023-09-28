9c6a1667d8760fd5a2c04e185e8b68613c49a410
eV <8796196+evq@users.noreply.github.com>
2023-09-28 18:05:39 +0000

    always return an AppError from submit endpoint (#2029)

--- libs/payments/submit.go
+++ libs/payments/submit.go
@@ -1,7 +1,5 @@
 package payments
 
-import ()
-
 // SubmitRequest is provided to indicate a payment that should be executed.
 type SubmitRequest struct {
 	DocumentID string `json:"documentId,omitempty"`
@@ -10,6 +8,5 @@ type SubmitRequest struct {
 // SubmitResponse is returned to provide the status of a payment after submission, along with any
 // error that resulted, if necessary.
 type SubmitResponse struct {
-	Status    PaymentStatus `json:"status" valid:"required"`
-	LastError *PaymentError `json:"error,omitempty"`
+	Status PaymentStatus `json:"status" valid:"required"`
 }
