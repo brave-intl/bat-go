64b48fe05123e81eab0b7dd3fd813a000495b41d
eV <8796196+evq@users.noreply.github.com>
2023-10-11 00:03:33 +0000

    add instructions for locally reproducing the PCR of a given docker image (#2089)
    * add instructions for locally reproducing the PCR of a given docker image
    
    * fixes #2084 by sorting env map keys before writing out
    
    * update Dockerfile

--- nitro-shim/tools/eifbuild/main.go
+++ nitro-shim/tools/eifbuild/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"path/filepath"
+	"sort"
 	"strings"
 	"text/template"
 )
@@ -53,7 +54,7 @@ files:
 
 func printusage() {
 	fmt.Println("Usage:\n")
-	fmt.Println("eifbuild -pass-envs ENVS -docker-uri IMAGE -output-file OUTPUT -- COMMAND...\n")
+	fmt.Println("eifbuild [-blobs-path BLOBS_PATH] -pass-envs ENVS -docker-uri IMAGE -output-file OUTPUT -- COMMAND...\n")
 
 	flag.PrintDefaults()
 }
@@ -69,6 +70,7 @@ func main() {
 	passEnvPtr := flag.String("pass-env", "", "Comma separated list of env vars to pass to the build")
 	imagePtr := flag.String("docker-uri", "", "Docker image URI")
 	outPtr := flag.String("output-file", "", "Output file for built EIF")
+	blobsPath := flag.String("blobs-path", "/usr/share/nitro_enclaves/blobs/", "Path to aws nitro cli blobs")
 	flag.BoolVar(&help, "help", false, "Show help")
 	flag.BoolVar(&help, "h", false, "Show help (shorthand)")
 	flag.Usage = printusage
@@ -118,7 +120,7 @@ func main() {
 
 	fmt.Println("\nBuilding...")
 
-	err := BuildEif("/usr/share/nitro_enclaves/blobs/", *imagePtr, cmd, envs, *outPtr)
+	err := BuildEif(*blobsPath, *imagePtr, cmd, envs, *outPtr)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -183,7 +185,18 @@ func BuildEif(blobsPath string, image string, cmds []string, envs map[string]str
 	for _, c := range cmds {
 		fmt.Fprintf(cmd, "%s\n", c)
 	}
-	for k, v := range envs {
+
+	// sort the env vars to ensure they are reproducible
+	keys := make([]string, len(envs))
+	i := 0
+	for k := range envs {
+		keys[i] = k
+		i++
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		v := envs[k]
 		fmt.Fprintf(env, "%s=%s\n", k, v)
 	}
 
@@ -226,7 +239,7 @@ func BuildEif(blobsPath string, image string, cmds []string, envs map[string]str
 	if err != nil {
 		return err
 	}
-	command = execCommand("/root/.cargo/bin/eif_build",
+	command = execCommand("eif_build",
 		"--kernel",
 		filepath.Join(blobsPath, "bzImage"),
 		"--kernel_config",
