87044622a6e25b74d669d72e1f5936ada76b37f8
eV <ev@7pr.xyz>
2023-10-13 19:05:01 +0000

    have worker use payments uri depending on environment

---
+++ libs/payments/api.go
@@ -0,0 +1,10 @@
+package payments
+
+var (
+	APIBase = map[string]string{
+		"":        "https://nitro-payments.bsg.brave.software",
+		"local":   "https://nitro-payments.bsg.brave.software",
+		"dev":     "https://nitro-payments.bsg.brave.software",
+		"staging": "https://nitro-payments-staging.bsg.brave.com",
+	}
+)
