1ec881884cf47fb86e087905e1270b778453d5e5
husobee <husobee@e-ie.io>
2023-11-06 15:42:10 -0500

    Husobee authorize key (#2201)
    * attempt to lock down authorizers keys in nitro payments
    
    * adding husobee authorize key for nitro-payments

--- services/payments/authorize.go
+++ services/payments/authorize.go
@@ -20,6 +20,8 @@ var validAuthorizerKeys = map[string][]string{
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA91/jZI+hcisdAURdqgdAKyetA4b2mVJIypfEtTyXW+ evq+settlements@brave.com",
 		// @sneagan
 		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDfcr9jUEu9D9lSpUnPwT1cCggCe48kZw1bJt+CXYSnh jegan+settlements@brave.com",
+		// @husobee
+		"ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOiLTWtWvC63Rkoip9yNUf7249w+RxR2PzG8O89KJsIs husobee+settlements@brave.com",
 	},
 	"development": {
 		// @kdenhartog for dev environment only
