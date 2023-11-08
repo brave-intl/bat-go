4c90e784252df1bc08fd03ae7d6fc0329351be68
Jackson <jegan@brave.com>
2023-11-08 17:06:08 -0500

    Adjust ALB bypass address

--- libs/payments/api.go
+++ libs/payments/api.go
@@ -2,9 +2,9 @@ package payments
 
 var (
 	APIBase = map[string]string{
-		"":        "https://web.payments-dev.svc.cluster.local",
-		"local":   "https://web.payments-dev.svc.cluster.local",
-		"dev":     "https://web.payments-dev.svc.cluster.local",
+		"":        "http://web.payment-dev.svc.cluster.local",
+		"local":   "http://web.payment-dev.svc.cluster.local",
+		"dev":     "http://web.payment-dev.svc.cluster.local",
 		"staging": "https://nitro-payments-staging.bsg.brave.com",
 	}
 )
