9fe090e700af452a5d034021344d812cf99f27e8
clD11 <23483715+clD11@users.noreply.github.com>
2024-01-22 21:29:58 +0000

    fix: add is allowed check to allow list entry

--- services/wallet/controllers_v4_test.go
+++ services/wallet/controllers_v4_test.go
@@ -606,6 +606,49 @@ func (suite *WalletControllersTestSuite) TestGetWalletV4() {
 	suite.Assert().Equal(true, resp.SelfCustodyAvailable["solana"])
 }
 
+func (suite *WalletControllersTestSuite) TestGetWalletV4_Not_Whitelisted() {
+	pg, _, err := wallet.NewPostgres()
+	suite.Require().NoError(err)
+
+	pub, _, err := ed25519.GenerateKey(nil)
+	suite.Require().NoError(err)
+
+	paymentID := uuid.NewV4()
+	w := &walletutils.Info{
+		ID:          paymentID.String(),
+		Provider:    "brave",
+		PublicKey:   hex.EncodeToString(pub),
+		AltCurrency: ptrTo(altcurrency.BAT),
+	}
+	err = pg.InsertWallet(context.TODO(), w)
+	suite.Require().NoError(err)
+
+	allowList := storage.NewAllowList()
+
+	service, _ := wallet.InitService(pg, nil, nil, allowList, nil, nil, nil, nil, nil, nil, wallet.DAppConfig{})
+
+	handler := handlers.AppHandler(wallet.GetWalletV4(service))
+
+	req, err := http.NewRequest("GET", "/v4/wallets/"+paymentID.String(), nil)
+	suite.Require().NoError(err, "a request should be created")
+
+	req = req.WithContext(context.WithValue(req.Context(), appctx.NoUnlinkPriorToDurationCTXKey, "-P1D"))
+	rctx := chi.NewRouteContext()
+	rctx.URLParams.Add("paymentID", paymentID.String())
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, rctx))
+
+	rr := httptest.NewRecorder()
+	handler.ServeHTTP(rr, req)
+
+	suite.Assert().Equal(http.StatusOK, rr.Code)
+
+	var resp wallet.ResponseV4
+	err = json.Unmarshal(rr.Body.Bytes(), &resp)
+	suite.Require().NoError(err)
+
+	suite.Assert().Equal(false, resp.SelfCustodyAvailable["solana"])
+}
+
 func signUpdateRequest(req *http.Request, paymentID string, privateKey ed25519.PrivateKey) error {
 	var s httpsignature.SignatureParams
 	s.Algorithm = httpsignature.ED25519
