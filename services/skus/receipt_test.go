e55e320f3633a9896de601a295b7236041317aab
PavelBrm <pburmistrov@brave.com>
2024-03-25 18:03:35 +1300

    test: more tests

--- services/skus/receipt_test.go
+++ services/skus/receipt_test.go
@@ -62,6 +62,10 @@ func TestReceiptVerifier_validateApple(t *testing.T) {
 				key: "key",
 				cl: &mockASClient{
 					fnVerify: func(ctx context.Context, req appstore.IAPRequest, result interface{}) error {
+						if req.Password == "" {
+							return model.Error("unexpected")
+						}
+
 						return model.Error("some_error")
 					},
 				},
@@ -75,6 +79,10 @@ func TestReceiptVerifier_validateApple(t *testing.T) {
 				key: "key",
 				cl: &mockASClient{
 					fnVerify: func(ctx context.Context, req appstore.IAPRequest, result interface{}) error {
+						if req.Password == "" {
+							return model.Error("unexpected")
+						}
+
 						return nil
 					},
 				},
@@ -191,6 +199,106 @@ func TestReceiptVerifier_validateApple(t *testing.T) {
 
 			actual, err := vrf.validateApple(context.Background(), tc.given.req)
 			must.Equal(t, true, errors.Is(err, tc.exp.err))
+
+			should.Equal(t, tc.exp.val, actual)
+		})
+	}
+}
+
+func TestFindInAppBySubID(t *testing.T) {
+	type tcGiven struct {
+		iap   []appstore.InApp
+		subID string
+	}
+
+	type tcExpected struct {
+		val *appstore.InApp
+		ok  bool
+	}
+
+	type testCase struct {
+		name  string
+		given tcGiven
+		exp   tcExpected
+	}
+
+	tests := []testCase{
+		{
+			name: "empty",
+			given: tcGiven{
+				iap:   []appstore.InApp{},
+				subID: "braveleo.monthly",
+			},
+			exp: tcExpected{},
+		},
+
+		{
+			name: "one_item_not_found",
+			given: tcGiven{
+				iap: []appstore.InApp{
+					{ProductID: "bravevpn.yearly"},
+				},
+
+				subID: "braveleo.monthly",
+			},
+			exp: tcExpected{},
+		},
+
+		{
+			name: "two_items_not_found",
+			given: tcGiven{
+				iap: []appstore.InApp{
+					{ProductID: "bravevpn.yearly"},
+					{ProductID: "bravevpn.monthly"},
+				},
+
+				subID: "braveleo.monthly",
+			},
+			exp: tcExpected{},
+		},
+
+		{
+			name: "one_item_found",
+			given: tcGiven{
+				iap: []appstore.InApp{
+					{ProductID: "braveleo.monthly"},
+				},
+
+				subID: "braveleo.monthly",
+			},
+			exp: tcExpected{
+				val: &appstore.InApp{ProductID: "braveleo.monthly"},
+				ok:  true,
+			},
+		},
+
+		{
+			name: "two_items_found",
+			given: tcGiven{
+				iap: []appstore.InApp{
+					{ProductID: "bravevpn.monthly"},
+					{ProductID: "braveleo.monthly"},
+				},
+
+				subID: "braveleo.monthly",
+			},
+			exp: tcExpected{
+				val: &appstore.InApp{ProductID: "braveleo.monthly"},
+				ok:  true,
+			},
+		},
+	}
+
+	for i := range tests {
+		tc := tests[i]
+
+		t.Run(tc.name, func(t *testing.T) {
+			actual, ok := findInAppBySubID(tc.given.iap, tc.given.subID)
+			must.Equal(t, tc.exp.ok, ok)
+
+			if !tc.exp.ok {
+				return
+			}
 
 			should.Equal(t, tc.exp.val, actual)
 		})
