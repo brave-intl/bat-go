b997926c213f727e1a40217e3a3269c27d4cc97c
Jackson <jegan@brave.com>
2024-05-02 17:43:59 -0400

    Fixed with testing

--- tools/payments/cmd/disaster/go.mod
+++ tools/payments/cmd/disaster/go.mod
@@ -11,32 +11,36 @@ toolchain go1.22.0
 require (
 	filippo.io/age v1.1.1
 	github.com/aws/aws-sdk-go v1.50.13
-	github.com/aws/aws-sdk-go-v2/config v1.18.19
-	github.com/aws/aws-sdk-go-v2/service/s3 v1.31.0
+	github.com/aws/aws-sdk-go-v2/config v1.27.11
+	github.com/aws/aws-sdk-go-v2/service/s3 v1.53.1
 	github.com/brave-intl/bat-go/libs v0.0.0-20230403164253-ad445ff37512
 	github.com/hashicorp/vault v1.16.2
 )
 
 require (
-	github.com/aws/aws-sdk-go-v2 v1.23.4 // indirect
-	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.10 // indirect
-	github.com/aws/aws-sdk-go-v2/credentials v1.13.18 // indirect
-	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.1 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/configsources v1.2.7 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.5.7 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/ini v1.3.32 // indirect
-	github.com/aws/aws-sdk-go-v2/internal/v4a v1.0.23 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.9.11 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.1.26 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.9.25 // indirect
-	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.14.0 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sso v1.12.6 // indirect
-	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.14.6 // indirect
-	github.com/aws/aws-sdk-go-v2/service/sts v1.18.7 // indirect
-	github.com/aws/smithy-go v1.18.1 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.26.1 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.2 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.11 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.1 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.16.15 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.5 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.5 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/v4a v1.3.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.2 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/checksum v1.3.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.7 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/s3shared v1.17.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/kms v1.31.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sqs v1.31.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.20.5 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.23.4 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.28.6 // indirect
+	github.com/aws/smithy-go v1.20.2 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
--- tools/payments/cmd/disaster/main.go
+++ tools/payments/cmd/disaster/main.go
@@ -1,9 +1,13 @@
 /*
-disaster is a disaster recovery command that will download the encrypted secrets file from S3 and decrypt it using locally-possessed shares.
+disaster is a disaster recovery command that will download the encrypted secrets
+file from S3 and decrypt it using locally-possessed shares. It also has an
+optional decrypt command that will print an unencrypted string of the share
+given a key and an encrypted share.
 
 Usage:
 
-disaster [flags] [shareFiles...]
+disaster [flags] [shares...]
+disaster [flags] decrypt shareFile
 
 The flags are:
 
@@ -21,17 +25,21 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/base64"
 	"flag"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"log"
 	"os"
 
 	"filippo.io/age"
+	"filippo.io/age/agessh"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/brave-intl/bat-go/libs/requestutils"
+	"github.com/brave-intl/bat-go/tools/payments"
 	"github.com/hashicorp/vault/shamir"
 )
 
@@ -66,34 +74,67 @@ func main() {
 		log.Fatal("insufficient shares provided")
 	}
 
-	disaster(ctx, *operatorKey, *s3Bucket, *s3Object, args, *env, *verbose)
+	if args[0] == "decrypt" {
+		log.Println("!!!! WARNING: THIS WILL REVEAL YOUR SECRET SHARE !!!!")
+		priv, err := payments.GetOperatorPrivateKey(*operatorKey)
+		if err != nil {
+			log.Fatalf("failed to open operator key file: %v\n", err.Error())
+		}
+
+		identity, err := agessh.NewEd25519Identity(priv)
+		if err != nil {
+			log.Fatalf("Failed to parse private key as identity: %v", err)
+		}
+
+		sf, err := os.Open(args[1])
+		if err != nil {
+			log.Fatalf("Failed to open file: %v", err)
+		}
+
+		r, err := age.Decrypt(sf, identity)
+		if err != nil {
+			log.Fatalf("Failed to open encrypted file: %v", err)
+		}
+
+		shareVal := &bytes.Buffer{}
+		if _, err := io.Copy(shareVal, r); err != nil {
+			log.Fatalf("Failed to read encrypted file: %v", err)
+		}
+
+		// s is the shamir share
+		log.Printf("Share: %s", shareVal.String())
+		return
+	}
+	var shares [][]byte
+	for _, encShare := range args {
+		share, err := base64.StdEncoding.DecodeString(string(encShare))
+		if err != nil {
+			log.Fatalf("failed to base64 decode operator key share: %w", err)
+		}
+		shares = append(shares, share)
+	}
+
+	disaster(ctx, *operatorKey, *s3Bucket, *s3Object, shares, *env, *verbose)
 }
 
-func disaster(ctx context.Context, key, bucket, file string, shareFilenames []string, env string, verbose bool) {
+func disaster(ctx context.Context, key, bucket, file string, shares [][]byte, env string, verbose bool) {
 	cfg, err := config.LoadDefaultConfig(ctx)
 	if err != nil {
 		log.Fatalf("failed to load default aws config: %v", err)
 	}
 	s3Client := s3.NewFromConfig(cfg)
+	log.Printf("fetching file %s from bucket %s", file, bucket)
 	output, err := s3Client.GetObject(ctx, &s3.GetObjectInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(file),
 	})
 	if err != nil {
-		log.Fatalf("failed to upload configuration: %v", err)
+		log.Fatalf("failed to fetch configuration: %v", err)
 	}
 
 	bodyBytes, err := requestutils.Read(ctx, output.Body)
 	if err != nil {
 		log.Fatalf("failed to read response body: %w", err)
-	}
-	var shares [][]byte
-	for _, f := range shareFilenames {
-		dat, err := os.ReadFile(f)
-		if err != nil {
-			log.Fatalf("failed to read share file: %w", err)
-		}
-		shares = append(shares, dat)
 	}
 	recoveredData, err := decryptRecoveryData(bodyBytes, shares)
 	if err != nil {
@@ -116,6 +157,7 @@ func decryptRecoveryData(encData []byte, shares [][]byte) ([]byte, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to combine keyShares: %w", err)
 	}
+	log.Printf("PRIVATE KEY: %s", privateKey)
 	identity, err := age.ParseX25519Identity(string(privateKey))
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse private key bytes for secret decryption: %w", err)
