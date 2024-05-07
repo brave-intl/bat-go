1edd2cc19fb78953c613a341a0d7a991b9563137
Jackson <jegan@brave.com>
2024-05-07 18:22:06 -0400

    Update DR CLI help text (#2493)

--- tools/payments/cmd/disaster/main.go
+++ tools/payments/cmd/disaster/main.go
@@ -20,8 +20,10 @@ The flags are:
 		The environment to which the operator is sending approval for transactions.
 	-b
 		The s3 bucket from which to retrieve the configuration
-	-f
-		The filename to get from the s3 bucket
+	-o
+		The object filename to get from the s3 bucket
+	-v
+		Verbose logging
 */
 package main
 
@@ -35,6 +37,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"filippo.io/age"
 	"filippo.io/age/agessh"
@@ -51,26 +54,19 @@ func main() {
 
 	// command line flags
 	operatorKey := flag.String(
-		"k", "test/private.pem",
-		"the operator's key file location (ed25519 private key) in PEM format")
-
+		"k",
+		"test/private.pem",
+		"the operator's key file location (ed25519 private key) in PEM format",
+	)
 	leaderPubkeyFile := flag.String(
-		"p", "",
-		"file containing the leader's pubkey for encrypting a given share for the purpose of secure transmission")
-
-	s3Bucket := flag.String("b", "", "the s3 bucket to upload to")
-
-	s3Object := flag.String(
-		"o", "",
-		"the s3 object name for output")
-
-	env := flag.String(
-		"e", "local",
-		"the environment to which the tool will interact")
-
-	verbose := flag.Bool(
-		"v", false,
-		"view verbose logging")
+		"p",
+		"",
+		"file containing the leader's pubkey for encrypting a given share for the purpose of secure transmission",
+	)
+	s3Bucket := flag.String("b", "", "the s3 bucket from which to retrieve the configuration")
+	s3Object := flag.String("o", "", "the filename to get from the s3 bucket")
+	env := flag.String("e", "local", "the environment to which the tool will interact")
+	verbose := flag.Bool("v", false, "view verbose logging")
 
 	flag.Parse()
 
@@ -91,9 +87,10 @@ func main() {
 			log.Fatalf("failed to parse public key %q: %w", leaderPubkey, err)
 		}
 
-		name := "for-leader"
-		// open output file for this operator
-		f, err := os.Create(fmt.Sprintf("share-%s.enc", name))
+		// open output file for this operator named for the email address on the pubkey
+		keyEmail := strings.Split(string(leaderPubkey), " ")
+		name := fmt.Sprintf("share-for-%s.enc", strings.TrimSpace(keyEmail[len(keyEmail)-1]))
+		f, err := os.Create(name)
 		if err != nil {
 			log.Fatalf("failed to open leader receipient share file", err.Error())
 		}
@@ -111,6 +108,8 @@ func main() {
 		if err := w.Close(); err != nil {
 			log.Fatalf("failed to close receipient share file", err.Error())
 		}
+
+		log.Printf("Wrote share encrypted for leader to %s", name)
 
 		return
 	}
@@ -152,8 +151,8 @@ func disaster(ctx context.Context, key, bucket, file string, shares [][]byte, en
 		log.Fatalf("failed to read attested recovery file: %v\n", err)
 	}
 
-	responseFile := "recovered-secrets.txt"
-	err = os.WriteFile(responseFile, recoveredData, 0644)
+	secretsFile := "recovered-secrets.txt"
+	err = os.WriteFile(secretsFile, recoveredData, 0644)
 	if err != nil {
 		log.Fatalf("failed to write recovery data to file: %w", err)
 	}
@@ -161,6 +160,7 @@ func disaster(ctx context.Context, key, bucket, file string, shares [][]byte, en
 	if verbose {
 		log.Println("disaster command complete")
 	}
+	log.Printf("Wrote secrets file to %s", secretsFile)
 }
 
 func decryptRecoveryData(encData []byte, shares [][]byte) ([]byte, error) {
@@ -209,5 +209,6 @@ func decryptShare(operatorKeyFile, shareFile string) string {
 	if _, err := io.Copy(shareVal, r); err != nil {
 		log.Fatalf("Failed to read encrypted file: %v", err)
 	}
+
 	return shareVal.String()
 }
