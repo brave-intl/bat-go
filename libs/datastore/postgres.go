a6f4624c1fcffab94b26f07df1370c2e3167b515
PavelBrm <pburmistrov@brave.com>
2024-05-09 22:58:30 +1200

    feat: change request_id to disallow null and change index

--- libs/datastore/postgres.go
+++ libs/datastore/postgres.go
@@ -41,7 +41,7 @@ var (
 	}
 	dbs = map[string]*sqlx.DB{}
 	// CurrentMigrationVersion holds the default migration version
-	CurrentMigrationVersion = uint(66)
+	CurrentMigrationVersion = uint(68)
 	// MigrationTracks holds the migration version for a given track (eyeshade, promotion, wallet)
 	MigrationTracks = map[string]uint{
 		"eyeshade": 20,
---
+++ migrations/0067_tlv2_request_id_not_null.down.sql
@@ -0,0 +1,1 @@
+ALTER TABLE time_limited_v2_order_creds ALTER COLUMN request_id DROP NOT NULL;
