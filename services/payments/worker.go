0d02eb5d10d5a80b3abf5eeb41269bf0ada8a4fd
Jackson <jegan@brave.com>
2024-05-09 14:47:16 -0400

    Increase worker retry interval for solana

--- services/payments/worker.go
+++ services/payments/worker.go
@@ -91,7 +91,7 @@ func (w *Worker) requestHandler(ctx context.Context, client *client.SimpleHTTPCl
 		Path: r.URL.RequestURI(),
 	})
 
-	delay := 1 * time.Second
+	delay := 30 * time.Second
 	resp, err := client.Do(ctx, r, nil)
 	if resp != nil {
 		retry := resp.Header.Get("x-retry-after")
