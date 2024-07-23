021bfe4ae24181971aca14079ae693101b04a9fc
clD11 <23483715+clD11@users.noreply.github.com>
2024-07-23 16:50:16 +0100

    style: change logging format

--- services/skus/controllers.go
+++ services/skus/controllers.go
@@ -703,9 +703,7 @@ func GetOrderCreds(service *Service) handlers.AppHandler {
 			)
 		}
 
-		l := logging.Logger(ctx, "skus").With().
-			Str("func", "GetOrderCreds").
-			Logger()
+		l := logging.Logger(ctx, "skus").With().Str("func", "GetOrderCreds").Logger()
 
 		creds, status, err := service.GetCredentials(ctx, *orderID.UUID())
 		if err != nil {
@@ -717,11 +715,7 @@ func GetOrderCreds(service *Service) handlers.AppHandler {
 				}
 				w.Header().Set("Retry-After", strconv.FormatInt(avg, 10))
 			} else {
-				l.Error().Err(err).
-					Str("orderID", orderID.String()).
-					Int("status", status).
-					Msg("failed to get order creds")
-
+				l.Error().Err(err).Str("orderID", orderID.String()).Int("status", status).Msg("failed to get order creds")
 				return handlers.WrapError(err, "Error getting credentials", status)
 			}
 		}
@@ -805,20 +799,13 @@ func getOrderCredsByID(svc *Service, legacyMode bool) handlers.AppHandler {
 			reqID = *reqIDRaw.UUID()
 		}
 
-		l := logging.Logger(ctx, "skus").With().
-			Str("func", "getOrderCredsByID").
-			Logger()
+		l := logging.Logger(ctx, "skus").With().Str("func", "getOrderCredsByID").Logger()
 
 		itemIDv := *itemID.UUID()
 		creds, status, err := svc.GetItemCredentials(ctx, *orderID.UUID(), itemIDv, reqID)
 		if err != nil {
 			if !errors.Is(err, errSetRetryAfter) {
-				l.Error().Err(err).
-					Str("orderID", orderID.String()).
-					Str("itemID", itemIDv.String()).
-					Int("status", status).
-					Msg("failed to get item creds")
-
+				l.Error().Err(err).Str("orderID", orderID.String()).Str("itemID", itemIDv.String()).Int("status", status).Msg("failed to get item creds")
 				return handlers.WrapError(err, "Error getting credentials", status)
 			}
 
