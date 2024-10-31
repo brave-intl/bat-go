ee8691af381ea8366d00a08664845c3af002f648
clD11 <23483715+clD11@users.noreply.github.com>
2024-10-31 08:25:32 +0000

    refactor: remove s3Client interface from rewards and cleanup (#2690)
    * refactor: remove s3Client interface from rewards and cleanup
    
    * refactor: format rewards get parameters code

--- services/rewards/controllers.go
+++ services/rewards/controllers.go
@@ -9,48 +9,38 @@ import (
 	"github.com/brave-intl/bat-go/libs/logging"
 )
 
-// GetParametersHandler - handler to get reward parameters
 func GetParametersHandler(service *Service) handlers.AppHandler {
-	return handlers.AppHandler(func(w http.ResponseWriter, r *http.Request) *handlers.AppError {
-		// get context from request
-		ctx := r.Context()
-
-		var (
-			currencyInput = r.URL.Query().Get("currency")
-			parameters    *ParametersV1
-			err           error
-		)
-
+	return func(w http.ResponseWriter, r *http.Request) *handlers.AppError {
+		var currencyInput = r.URL.Query().Get("currency")
 		if currencyInput == "" {
 			currencyInput = "USD"
 		}
 
-		// get logger from context
-		logger := logging.Logger(ctx, "rewards.GetParametersHandler")
+		ctx := r.Context()
 
-		// in here we need to validate our currency
-		var currency = new(BaseCurrency)
-		if err = inputs.DecodeAndValidate(ctx, currency, []byte(currencyInput)); err != nil {
+		lg := logging.Logger(ctx, "rewards").With().Str("func", "GetParametersHandler").Logger()
+
+		currency := new(BaseCurrency)
+		if err := inputs.DecodeAndValidate(ctx, currency, []byte(currencyInput)); err != nil {
+			lg.Error().Err(err).Msg("failed decode and validate")
+
 			if errors.Is(err, ErrBaseCurrencyInvalid) {
-				logger.Error().Err(err).Msg("invalid currency input from caller")
-				return handlers.ValidationError(
-					"Error validating currency url parameter",
-					map[string]interface{}{
-						"err":      err.Error(),
-						"currency": "invalid currency",
-					},
-				)
+				return handlers.ValidationError("Error validating currency url parameter", map[string]interface{}{
+					"err":      err.Error(),
+					"currency": "invalid currency",
+				})
 			}
-			// degraded, unknown error when validating/decoding
-			logger.Error().Err(err).Msg("unforseen error in decode and validation")
+
 			return handlers.WrapError(err, "degraded: ", http.StatusInternalServerError)
 		}
 
-		parameters, err = service.GetParameters(ctx, currency)
+		parameters, err := service.GetParameters(ctx, currency)
 		if err != nil {
-			logger.Error().Err(err).Msg("failed to get reward parameters")
+			lg.Error().Err(err).Msg("failed to get reward parameters")
+
 			return handlers.WrapError(err, "failed to get parameters", http.StatusInternalServerError)
 		}
+
 		return handlers.RenderContent(ctx, parameters, w, http.StatusOK)
-	})
+	}
 }
