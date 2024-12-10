cbc8686538e1a0fb88604eeac5a64275fb9ba9fa
PavelBrm <pburmistrov@brave.com>
2024-12-10 20:06:44 +1300

    fix: bring back special handling for leo

--- services/skus/service.go
+++ services/skus/service.go
@@ -2106,7 +2106,17 @@ func (s *Service) redeemBlindedCred(ctx context.Context, w http.ResponseWriter, 
 	// FIXME: we shouldn't be using the issuer as the payload, it ideally would be a unique request identifier
 	// to allow for more flexible idempotent behavior.
 	if err := redeemFn(ctx, cred.Issuer, cred.TokenPreimage, cred.Signature, cred.Issuer); err != nil {
-		return handleRedeemFnError(ctx, w, kind, cred, err)
+		if !shouldRetryRedeemFn(kind, cred.Issuer, err) {
+			return handleRedeemFnError(ctx, w, kind, cred, err)
+		}
+
+		// TODO: remove this as there should be no credentials in Production signed by brave-leo-premium-year.
+		//
+		// Fix for https://github.com/brave-intl/challenge-bypass-server/pull/371.
+		const leoa = "brave.com?sku=brave-leo-premium-year"
+		if err := redeemFn(ctx, leoa, cred.TokenPreimage, cred.Signature, cred.Issuer); err != nil {
+			return handleRedeemFnError(ctx, w, kind, cred, err)
+		}
 	}
 
 	// TODO(clD11): cleanup after quick fix
@@ -2885,6 +2895,12 @@ func handleRedeemFnError(ctx context.Context, w http.ResponseWriter, kind string
 	}
 
 	return handlers.WrapError(err, "Error verifying credentials", http.StatusInternalServerError)
+}
+
+func shouldRetryRedeemFn(kind, issuer string, err error) bool {
+	const leo = "brave.com?sku=brave-leo-premium"
+
+	return kind == timeLimitedV2 && issuer == leo && err.Error() == cbr.ErrBadRequest.Error()
 }
 
 func newRadomGateway(env string) (*radom.Gateway, error) {
